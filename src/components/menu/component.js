af0e32d5aa9e8e34ba01d7da0afde1e1c33b3720
Cristian Barrientos Montoya <cs3kns@gmail.com>
2016-03-30 17:59:15 -0500

    Navbar and dashboard components creation.

--- src/components/menu/component.js
+++ src/components/menu/component.js
@@ -4,9 +4,11 @@ import MenuList from './MenuList';
 class MenuComponent extends Component {
     render() {
         return (
-            <div className="page-sidebar mini" id="main-menu">
-                <MenuList/>
-                <div id="submenu-container" className="inner-menu hidden">
+            <div className="page-container row-fluid">
+                <div className="page-sidebar" id="main-menu">
+                    <MenuList/>
+                    <div id="submenu-container" className="inner-menu hidden">
+                    </div>
                 </div>
             </div>
         );
