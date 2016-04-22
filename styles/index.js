0d549f1bebb12492f575299c6a5526ca3dc09301
tmontoya-ias <tmontoya@ias.com.co>
2016-04-22 18:25:10 -0500

    Edit client

--- styles/index.js
+++ styles/index.js
@@ -4,4 +4,4 @@ require('./biztrack/modals.scss');
 require('./biztrack/_closeBt4.scss');
 require('./biztrack/tables.scss');
 
-import 'react-widgets/lib/less/react-widgets.less';
+require('react-widgets/lib/less/react-widgets.less');
