6c18f39db8dac6da39ca855f2b2ca17d34a19ce4
mafesernaarboleda <mafesernaarboleda@gmail.com>
2016-04-28 15:51:26 -0500

    implementación crear contacto

--- src/components/contact/createContact/reducer.js
+++ src/components/contact/createContact/reducer.js
@@ -2,7 +2,11 @@ import * as actions from './constants';
 import Immutable from 'immutable';
 
 const initialState = Immutable.Map({
-    modalState: false
+    modalState: false,
+    status: false,
+    responseCreateContact : {},
+    validateLogin: false,
+    responseSearchContactData:{}
 });
 
 export default function(state = initialState, action){
@@ -11,6 +15,17 @@ export default function(state = initialState, action){
         const actualState = state.get('modalState');
         const newState = actualState === false ? true : false;
         return state.set('modalState', newState);
+    case actions.CREATE_CONTACT:
+            const {responseCreateContact} = action.payload.data;
+            return state.set('responseCreateContactData', responseCreateContact);
+    case actions.SEARCH_CONTACT:
+            const response = action.payload.data;
+            return state.withMutations(map => {
+                map
+                .set('status', response.status)
+                .set('validateLogin', response.validateLogin)
+                .set('responseSearchContactData', JSON.parse(response.contactDetail));
+            });
     default:
         return state;
     }
