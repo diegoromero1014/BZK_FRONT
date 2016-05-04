482a9e588107f8b090dc412305bf672989b15dfd
wsalazar-ias <wsalazar@ias.com.co>
2016-05-04 11:19:04 -0500

    Ajuste comboBox component

--- src/components/shareholder/createShareholder/actions.js
+++
@@ -1,9 +0,0 @@
-import * as actions from './constants';
-import axios from 'axios';
-import {APP_URL} from '../../../constantsGlobal';
-
-export function toggleModalShareholder(){
-    return {
-        type: actions.TOGGLE_MODAL_SHAREHOLDER
-    };
-}
