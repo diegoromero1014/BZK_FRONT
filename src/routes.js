f1e460890e48174e73342590b829e937e7ec1c27
Tatiana Montoya <tatianamontoya15@gmail.com>
2016-06-08 15:08:22 -0500

    Carpeta de editar visita creada

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ import CreatePropspect from './components/propspect/createPropspect';
 import ClientEdit from './components/clientEdit/clientEdit';
 import Previsita from './components/previsita/createPrevisita/createPrevisita';
 import Visit from './components/visit/createVisit/createVisit';
+import VisitEdit from './components/visit/editVisit/editVisit';
 import uiTester from './ui/uiTester';
 
 class App extends Component {
@@ -38,6 +39,7 @@ export default (
                 <Route path="clientEdit" component={ClientEdit}></Route>
                 <Route path="previsita" component={Previsita}></Route>
                 <Route path="visita" component={Visit}></Route>
+                <Route path="visitaEditar" component={VisitEdit}></Route>
             </Route>
             <Route path="ui" component={uiTester}></Route>
         </Route>
