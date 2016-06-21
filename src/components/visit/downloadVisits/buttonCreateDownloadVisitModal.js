471671138743bd30fb54e33c4d1ae1339b7fa190
mafesernaarboleda <mafesernaarboleda@gmail.com>
2016-06-21 14:20:54 -0500

    modificación estilos opciones descarga excel.

--- src/components/visit/downloadVisits/buttonCreateDownloadVisitModal.js
+++ src/components/visit/downloadVisits/buttonCreateDownloadVisitModal.js
@@ -23,17 +23,17 @@ class ButtonCreateDownloadVisitModal extends Component {
 
 	render() {
 		return (
-      <div style={{position: 'fixed', border: '1px solid rgb(194, 194, 194)', bottom: '0px', width: '100%', marginBottom: '0px', height: '50px', background: 'rgba(255, 255, 255, 0.74902)'}}>
+      <div style={{marginLeft: '-20px',position: 'fixed', border: '1px solid rgb(194, 194, 194)', bottom: '0px', width: '100%', marginBottom: '0px', height: '50px', background: 'rgba(255, 255, 255, 0.74902)'}}>
 			<div style={{width: '580px', height: '100%', position: 'fixed', right: '0px'}}>
         <button className="btn" style={{float: 'right', margin: '8px 0px 0px 400px', position: 'fixed'}} title={'Exportar en formato CSV'} onClick={this.openModal}>
           <span style={{color: "#FFFFFF", padding:"10px"}}>{'Descargar '}<i className="file excel outline icon"></i></span>
         </button>
       </div>
       <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.closeModal} className="modalBt4-fade modal fade contact-detail-modal in">
-        <div className="modalBt4-dialog modalBt4-lg">
+        <div className="modalBt4-dialog ">
           <div className="modalBt4-content modal-content">
             <div className="modalBt4-header modal-header">
-              <h4 className="modal-title" style={{float: 'left', marginBottom: '0px'}} id="myModalLabel">{'opciones'}</h4>	
+              <h4 className="modal-title" style={{float: 'left', marginBottom: '0px'}} id="myModalLabel">{'Opciones'}</h4>
               <button type="button" onClick={this.closeModal} className="close" data-dismiss="modal" role="close">
                 <span className="modal-title" aria-hidden="true" role="close"><i className="remove icon modal-icon-close" role="close"></i></span>
                 <span className="sr-only">Close</span>
--- src/components/visit/downloadVisits/component.js
+++ src/components/visit/downloadVisits/component.js
@@ -45,9 +45,7 @@ class DownloadVisits extends Component {
 	_downloadVisits() {
 		const {getCsvVisitsByClient, isOpen} = this.props;
 		getCsvVisitsByClient(window.localStorage.getItem('idClientSelected'), this.state.hasParticipatingContacts, this.state.hasParticipatingEmployees, this.state.hasRelatedEmployees).then(function(data) {
-			console.log(data);
 			if (data.payload.data.status == 200) {
-
 				window.open(APP_URL + '/getCsvReport?filename=' + data.payload.data.data, '_blank');
 				isOpen();
 			}
@@ -57,9 +55,17 @@ class DownloadVisits extends Component {
 	render() {
 		return (
 			<div>
-				<div className="modalBt4-body modal-body business-content editable-form-content clearfix" id="modalComponentScroll">
-					<dt className="business-title"><span style={{paddingLeft: '20px'}}>{'En esta sección podrá descargar algunos campos  de los "informes de visita/reunión" del cliente.\n Seleccione los campos que desea descargar a excel:'}</span></dt>
-					<div style={{paddingLeft:'20px', paddingRight:'20px'}}>
+				<div style={{height: 'auto'}}className="modalBt4-body modal-body business-content editable-form-content clearfix" id="modalComponentScroll">
+					<div style={{paddingLeft:'20px', paddingRight:'20px', paddingTop: '20px'}}>
+					<span>{'En esta sección podrá descargar algunos campos  de los "informes de visita/reunión" del cliente.\n Seleccione los campos que desea descargar a excel:'}</span>
+						<ul className="ui list" style={{marginLeft:'0px'}}>
+							<div className="item"><input name="participatingContacts" type="checkbox" onChange={this._checkCheckBox} /> {'Participantes en la reunión por parte del cliente'}</div>
+							<div className="item"><input name="participatingEmployees" type="checkbox" onChange={this._checkCheckBox} /> {'Participantes en la reunión por parte del Grupo Bancolombia'}</div>
+							<div className="item"><input name="relatedEmployees" type="checkbox" onChange={this._checkCheckBox} /> {'Otros participantes en la reunión'}</div>
+						</ul>
+					</div>
+					<div style={{paddingLeft: '20px', paddingRight: '20px', paddingTop: '20px'}}>
+						<span>{'Adicional a los campos seleccionados, en el excel encontrará los siguiente campos:'}</span>
 						<ul className="ui list">
 							<li>{'Tipo de documento'}</li>
 							<li>{'Número de documento'}</li>
@@ -68,14 +74,6 @@ class DownloadVisits extends Component {
 							<li>{'Célula'}</li>
 							<li>{'Fecha de la reunión'}</li>
 							<li>{'Tipo de reunión'}</li>
-						</ul>
-					</div>
-					<dt className="business-title"><span style={{paddingLeft: '20px'}}>{'Adicional a los campos seleccionados, en el excel encontrará los siguiente campos:'}</span></dt>
-					<div style={{paddingLeft: '20px', paddingRight: '20px'}}>
-						<ul className="ui list">
-							<div className="item"><input name="participatingContacts" type="checkbox" onChange={this._checkCheckBox} /> {'Participantes en la reunión por parte del cliente'}</div>
-							<div className="item"><input name="participatingEmployees" type="checkbox" onChange={this._checkCheckBox} /> {'Participantes en la reunión por parte del Grupo Bancolombia'}</div>
-							<div className="item"><input name="relatedEmployees" type="checkbox" onChange={this._checkCheckBox} /> {'Otros participantes en la reunión'}</div>
 						</ul>
 					</div>
 				</div>
