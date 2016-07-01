54475b4b63ae9423292a3c6b5b3c6870ae9cf3b8
César Meneses <camenesesg@gmail.com>
2016-07-01 18:33:29 -0500

    Se organizan estilos del boton de previsita guia y la ventana modal de descarga de previsita

--- src/components/previsita/downloadPrevisits/component.js
+++ src/components/previsita/downloadPrevisits/component.js
@@ -46,28 +46,30 @@ class DownloadPrevisits extends Component {
 		return (
 			<div>
 				<div style={{height: 'auto'}} className="modalBt4-body modal-body business-content editable-form-content clearfix" id="modalComponentScroll">
-					<span>{'En esta sección podrá descargar algunos campos de los "informes de previsita del cliente.\n Seleccione los campos que desea descargar a excel:"'}</span>
-					<ul className="ui list" style={{marginLeft:'0px'}}>
-						<div className="item"><input name="participatingContacts" type="checkbox" onChange={this._checkCheckBox} /> {'Participantes en la reunión por parte del cliente'}</div>
-						<div className="item"><input name="participatingEmployees" type="checkbox" onChange={this._checkCheckBox} /> {'Participantes en la reunión por parte del Grupo Bancolombia'}</div>
-						<div className="item"><input name="relatedEmployees" type="checkbox" onChange={this._checkCheckBox} /> {'Otros participantes en la reunión'}</div>
-					</ul>
-				</div>
-				<div style={{paddingLeft: '20px', paddingRight: '20px', paddingTop: '20px'}}>
-					<span>{'Adicional a los campos seleccionados, en el excel encontrará los siguiente campos:'}</span>
-					<ul className="ui list">
-						<li>{'Tipo de documento'}</li>
-						<li>{'Número de documento'}</li>
-						<li>{'Razón social'}</li>
-						<li>{'Gerente de cuenta'}</li>
-						<li>{'Célula'}</li>
-						<li>{'Fecha de la reunión'}</li>
-						<li>{'Tipo de reunión'}</li>
-					</ul>
-				</div>
-				<div className="modalBt4-footer modal-footer">
-					<button type="submit" className="btn btn-primary modal-button-edit" onClick={this._downloadPreVisits}>{'Descargar '}<i className="file excel outline icon"></i></button>
-				</div>
+					<div style={{paddingLeft:'20px', paddingRight:'20px', paddingTop: '20px'}}>
+						<span>{'En esta sección podrá descargar algunos campos de los "informes de previsita del cliente.\n Seleccione los campos que desea descargar a excel:"'}</span>
+						<ul className="ui list" style={{marginLeft:'0px'}}>
+							<div className="item"><input name="participatingContacts" type="checkbox" onChange={this._checkCheckBox} /> {'Participantes en la reunión por parte del cliente'}</div>
+							<div className="item"><input name="participatingEmployees" type="checkbox" onChange={this._checkCheckBox} /> {'Participantes en la reunión por parte del Grupo Bancolombia'}</div>
+							<div className="item"><input name="relatedEmployees" type="checkbox" onChange={this._checkCheckBox} /> {'Otros participantes en la reunión'}</div>
+						</ul>
+					</div>
+					<div style={{paddingLeft: '20px', paddingRight: '20px', paddingTop: '20px'}}>
+						<span>{'Adicional a los campos seleccionados, en el excel encontrará los siguiente campos:'}</span>
+						<ul className="ui list">
+							<li>{'Tipo de documento'}</li>
+							<li>{'Número de documento'}</li>
+							<li>{'Razón social'}</li>
+							<li>{'Gerente de cuenta'}</li>
+							<li>{'Célula'}</li>
+							<li>{'Fecha de la reunión'}</li>
+							<li>{'Tipo de reunión'}</li>
+						</ul>
+					</div>
+					</div>
+					<div className="modalBt4-footer modal-footer">
+						<button type="submit" className="btn btn-primary modal-button-edit" onClick={this._downloadPreVisits}>{'Descargar '}<i className="file excel outline icon"></i></button>
+					</div>
 			</div>
 		);
 	}
