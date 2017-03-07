7872b768bdc85b9c254baa5377f74949017ea92b
ahurtado-ias <ahurtado@ias.com.co>
2017-03-07 14:14:53 -0500

    se realiza corrección de error en busqueda de nombre o nit en la alerta de lista de control.

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require("./webpack/webpack.config.dev");
-
+const IP_ENVIRONMENT = 'localhost';
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true
 })
-    .listen(3000, 'localhost', function (err, result) {
+    .listen(3000, IP_ENVIRONMENT, function (err, result) {
         if (err) {
             console.log(err);
         }
-
-        console.log('Listening at localhost:3000');
+        console.log('Listening at '+IP_ENVIRONMENT+':3000');
     });
