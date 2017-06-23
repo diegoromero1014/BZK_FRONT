341f3c824f8f245b52bdedff169baaaef835f8b6
wsalazag21729 <wilfersalazar19@gmail.com>
2017-06-23 17:26:00 -0500

    Cambio de grupo económico desde el front, se elimina el grupo económico del cliente y se crea el documento como NIT principal

--- src/components/clientEconomicGroup/actions.js
+++ src/components/clientEconomicGroup/actions.js
@@ -1,5 +1,5 @@
 import {APP_URL} from '../../constantsGlobal';
-import {CONSULT_ECONOMIC_GROUP} from './constants';
+import {CONSULT_ECONOMIC_GROUP, DELETE_RETAIONSHIP_ECONOMIC_GRUOP} from './constants';
 import axios from 'axios';
 
 export function getClientsEconomicGroup(id){
@@ -20,10 +20,33 @@ export function getClientsEconomicGroup(id){
       "groupId": id
     }
   }
-
   var request = axios.post(APP_URL + "/getGroupTeam", json);
   return{
     type: CONSULT_ECONOMIC_GROUP,
+    payload: request
+  }
+}
+
+export function deleteRelationEconomicGroup(idClient){
+  const json = {
+    "messageHeader":{
+      "sessionToken": window.localStorage.getItem('sessionToken'),
+      "timestamp": new Date().getTime(),
+      "service": "",
+      "status": "0",
+      "language": "es",
+      "displayErrorMessage": "",
+      "technicalErrorMessage": "",
+      "applicationVersion": "",
+      "debug": true,
+      "isSuccessful": true
+    },
+    "messageBody": idClient
+  }
+
+  var request = axios.post(APP_URL + "/deleteRelationEconomicGroup", json);
+  return{
+    type: DELETE_RETAIONSHIP_ECONOMIC_GRUOP,
     payload: request
   }
 }
--- src/components/clientEconomicGroup/clientsEconomicGroup.js
+++ src/components/clientEconomicGroup/clientsEconomicGroup.js
@@ -1,35 +1,86 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {Row, Grid, Col} from 'react-flexbox-grid';
-import {reduxForm} from 'redux-form';
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Row, Grid, Col } from 'react-flexbox-grid';
+import { reduxForm } from 'redux-form';
 import SweetAlert from 'sweetalert-react';
-import numeral from 'numeral';
-import _ from 'lodash';
+import { getClientsEconomicGroup, deleteRelationEconomicGroup } from './actions';
+import { swtShowMessage } from '../sweetAlertMessages/actions';
+import { validateResponse } from '../../actionsGlobal';
+import { MESSAGE_SAVE_DATA } from '../../constantsGlobal';
 
 class clientsEconomicGroup extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showConfirmDelete: false
+    };
+    this._removeClientOfEconomicGroup = this._removeClientOfEconomicGroup.bind(this);
   }
 
-  render(){
-    const {dataName, dataDocumentType, dataDocument, dataEconomicGroup, dataAccountManager, dataIsProspect, dataIsAccess} = this.props;
+  _removeClientOfEconomicGroup() {
+    this.setState({ showConfirmDelete: false });
+    const { deleteRelationEconomicGroup, getClientsEconomicGroup, clientInformacion, dataId, swtShowMessage, changeStateSaveData } = this.props;
+    deleteRelationEconomicGroup(dataId).then((data) => {
+      if (validateResponse(data)) {
+        const infoClient = clientInformacion.get('responseClientInfo');
+        swtShowMessage('success', 'Cliente eliminado', 'Señor usuario, el cliente fue eliminado correctamente del grupo económico');
+        getClientsEconomicGroup(infoClient.economicGroup);
+      } else {
+        swtShowMessage('error', 'Error eliminando relación', 'Señor usuario, ocurrió un error tratando de eliminar el cliente del grupo económico');
+      }
+    });
+  }
+
+  render() {
+    const { dataName, dataDocumentType, dataDocument, dataEconomicGroup, dataAccountManager, dataIsProspect, dataIsAccess } = this.props;
     return (
-      <div className="client-card" style={{width:"265px", float:"left"}}>
+      <div className="client-card" style={{ width: "265px", float: "left" }}>
         <div className="celula-card-top">
           <div className="celula-card-top-left">
             <div className="celula-title">{dataName.length > 60 ? dataName.substring(0, 60) + "..." : dataName}</div>
             <div className="celula-name">{dataDocumentType}: {dataDocument.length > 20 ? dataDocument.substring(0, 20) + "..." : dataDocument}</div>
-            <div className="celula-name" style={{marginTop: "5px", fontStyle: "italic"}}>{dataAccountManager}</div>
+            <div className="celula-name" style={{ marginTop: "5px", fontStyle: "italic" }}>{dataAccountManager}</div>
           </div>
         </div>
-        <div className="celula-card-bottom" style={{backgroundColor:dataIsAccess ? "#B0E0E6" : "#DCDCDC"}}>
+        <div className="celula-card-bottom" style={{ backgroundColor: dataIsAccess ? "#B0E0E6" : "#DCDCDC" }}>
+          <i className="trash outline icon" style={{ marginTop: "-14px", fontSize: '13pt' }}
+            onClick={() => this.setState({ showConfirmDelete: true })}
+            title="Eliminar cliente del grupo económico" />
         </div>
         {dataIsProspect &&
-        <div className="prospect-corner prospect badge badge-important animated bounceIn" style={{borderRadius:"10px"}}>P</div>
+          <div className="prospect-corner prospect badge badge-important animated bounceIn" style={{ borderRadius: "10px" }}>P</div>
         }
+        <SweetAlert
+          type="warning"
+          show={this.state.showConfirmDelete}
+          title="Grupo económico"
+          confirmButtonColor='#2671d7'
+          confirmButtonText='Sí'
+          cancelButtonText="No"
+          text="¿Señor usuario, está seguro que desea eliminar el cliente del grupo económico?"
+          showCancelButton={true}
+          onCancel={() => this.setState({ showConfirmDelete: false })}
+          onConfirm={() => this._removeClientOfEconomicGroup()} />
       </div>
     );
   }
 }
 
-export default clientsEconomicGroup;
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    deleteRelationEconomicGroup,
+    getClientsEconomicGroup,
+    swtShowMessage
+  }, dispatch);
+}
+
+function mapStateToProps({ clientInformacion, clientEconomicGroupReducer }, ownerProps) {
+  return {
+    clientInformacion,
+    clientEconomicGroupReducer
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(clientsEconomicGroup);
