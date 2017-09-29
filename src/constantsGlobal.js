067196ab97bafcf083a2dd18d6256cbb8584c88a
wasalaza <wilfersalazar19@gmail.com>
2017-09-29 09:09:15 -0500

    ajuste querys

--- src/constantsGlobal.js
+++ src/constantsGlobal.js
@@ -1,4 +1,4 @@
-export const VERSION_DATE = "26/09/2017";
+export const VERSION_DATE = "29/09/2017";
 export const PERSONA_NATURAL = 451;
 export const PERSONA_JURIDICA = 452;
 export const FILE_OPTION_SOCIAL_STYLE_CONTACT = 1;
