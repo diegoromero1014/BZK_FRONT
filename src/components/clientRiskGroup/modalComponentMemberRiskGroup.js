d04b765c17ca920a68fcfd431b65f3c7f68d0bb8
ahurtado-ias <ahurtado@ias.com.co>
2017-11-03 11:50:21 -0500

    Se realiza ajuste para que se muestren los tipo de documento de accionistas al momento de agregar clientes al grupo de riesgo y crear clientes externos

--- src/components/clientRiskGroup/modalComponentMemberRiskGroup.js
+++ src/components/clientRiskGroup/modalComponentMemberRiskGroup.js
@@ -1,271 +1,246 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Row, Col } from 'react-flexbox-grid';
-import { redirectUrl } from '../globalComponents/actions';
-import { reduxForm } from 'redux-form';
+import React, {Component} from 'react';
+import {Row, Col} from 'react-flexbox-grid';
+import {redirectUrl} from '../globalComponents/actions';
+import {reduxForm} from 'redux-form';
 import Input from '../../ui/input/inputComponent';
 import ComboBox from '../../ui/comboBox/comboBoxComponent';
-import Textarea from '../../ui/textarea/textareaComponent';
-
 import SweetAlert from 'sweetalert-react';
-import { swtShowMessage } from '../sweetAlertMessages/actions';
-import { REQUEST_ERROR, ERROR_MESSAGE_REQUEST, MESSAGE_USER_WITHOUT_PERMISSIONS, MESSAGE_LOAD_DATA, VALUE_REQUIERED, SESSION_EXPIRED } from '../../constantsGlobal';
-import { stringValidate, validateValueExist, validateResponse, formValidateKeyEnter, nonValidateEnter } from '../../actionsGlobal';
-import { bindActionCreators } from 'redux';
-
-
+import {swtShowMessage} from '../sweetAlertMessages/actions';
+import {VALUE_REQUIERED, SESSION_EXPIRED} from '../../constantsGlobal';
+import {validateResponse, formValidateKeyEnter, nonValidateEnter} from '../../actionsGlobal';
+import {bindActionCreators} from 'redux';
 import * as constants from '../selectsComponent/constants';
-
-import { findClientByTypeAndNumber } from '../clients/actions';
-import { consultDataSelect, consultList } from '../selectsComponent/actions';
-
-import ClientsRiskGroup from './clientsRiskGroup';
-import { showLoading } from '../loading/actions';
-
+import {findClientByStrTypeIdAndNumber} from '../clients/actions';
+import {getMasterDataFields} from '../selectsComponent/actions';
+import {showLoading} from '../loading/actions';
 import MemberRiskGroup from './memberRiskGroup'
-
 import _ from 'lodash';
-import $ from 'jquery';
-
 
 const fields = [
-  "idType", "idNumber"
+    "idType", "idNumber"
+];
 
-]
 const validate = values => {
-  const errors = {};
+    const errors = {};
 
-  if (!values.idType) {
-    errors.idType = VALUE_REQUIERED;
-  } else {
-    errors.idType = null;
-  }
-
-  if (!values.idNumber) {
-    errors.idNumber = VALUE_REQUIERED;
-  } else {
-    errors.idNumber = null;
-  }
-
-  return errors;
+    if (!values.idType) {
+        errors.idType = VALUE_REQUIERED;
+    } else {
+        errors.idType = null;
+    }
+    if (!values.idNumber) {
+        errors.idNumber = VALUE_REQUIERED;
+    } else {
+        errors.idNumber = null;
+    }
+    return errors;
 };
 
 let thisForm;
 class modalComponentMemberRiskGroup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showError: false,
-      showErrorForm: false,
-      showForm: false,
-      showConfirmCreateUser: false,
-      disabledPrimaryFields: false,
-      clientsBasicInfo: {}
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            showError: false,
+            showErrorForm: false,
+            showForm: false,
+            showConfirmCreateUser: false,
+            disabledPrimaryFields: false,
+            clientsBasicInfo: {}
+        };
 
-    this._handlerSubmitGroup = this._handlerSubmitGroup.bind(this);
-    this._closeError = this._closeError.bind(this);
-    this._onchangeValue = this._onchangeValue.bind(this);
-    this.confirmExtClients = this.confirmExtClients.bind(this);
-    thisForm = this;
-  }
-
-  _closeError() {
-    this.setState({ showError: false, messageError: '' });
-  }
-
-  componentWillMount() {
-    const { consultDataSelect, consultList } = this.props;
-
-    this.setState({ showForm: false });
-
-    consultDataSelect(constants.CLIENT_ID_TYPE).then((data) => {
-      if (_.get(data, 'payload.data.messageHeader.status') === SESSION_EXPIRED) {
-        redirectUrl("/login");
-      }
-    });
-
-  }
-
-  _onchangeValue(file, val) {
-
-    switch (file) {
-      case "idType":
-        let { fields: { idType } } = this.props;
-        idType.onChange(val);
-        break;
-      case "idNumber":
-        let { fields: { idNumber } } = this.props;
-        idNumber.onChange(val);
-        break;
-
-      default:
-        break;
+        this._handlerSubmitGroup = this._handlerSubmitGroup.bind(this);
+        this._closeError = this._closeError.bind(this);
+        this._onchangeValue = this._onchangeValue.bind(this);
+        this.confirmExtClients = this.confirmExtClients.bind(this);
+        thisForm = this;
     }
-  }
 
+    _closeError() {
+        this.setState({showError: false, messageError: ''});
+    }
 
-  _handlerSubmitGroup() {
-    const { fields: { idType, idNumber }, swtShowMessage, isOpen, findClientByTypeAndNumber } = this.props;
-
-    const jsonUpdateGroup = {
-      typeDocument: idType.value !== undefined ? idType.value : null,
-      numberDocument: idNumber.value !== undefined ? idNumber.value : null
-    };
-    const self = this;
-
-    findClientByTypeAndNumber(jsonUpdateGroup).then((data) => {
-      if (validateResponse(data)) {
-        this.setState({
-          showForm: true,
-          disabledPrimaryFields: true,
-          clientsBasicInfo: _.get(data, 'payload.data.data', {})
+    componentWillMount() {
+        this.setState({showForm: false});
+        this.props.getMasterDataFields([constants.SHAREHOLDER_ID_TYPE]).then((data) => {
+            if (_.get(data, 'payload.data.messageHeader.status') === SESSION_EXPIRED) {
+                redirectUrl("/login");
+            }
         });
-      } else {
-        swtShowMessage('error', 'Error consultando el cliente', 'Señor usuario, ocurrió un error tratando de consultar el cliente.');
-      }
 
-    }, (reason) => {
-      this.setState({ showConfirmCreateUser: true });
-    })
-  }
-
-  confirmExtClients(confirm) {
-    if (confirm) {
-      this.setState({
-        showConfirmCreateUser: false,
-        disabledPrimaryFields: true,
-        showForm: true,
-        clientsBasicInfo: {}
-      })
-    } else {
-      this.setState({
-        showConfirmCreateUser: false
-      })
     }
-  }
 
-  render() {
+    _onchangeValue(file, val) {
+        switch (file) {
+            case "idType":
+                let {fields: {idType}} = this.props;
+                idType.onChange(val);
+                break;
+            case "idNumber":
+                let {fields: {idNumber}} = this.props;
+                idNumber.onChange(val);
+                break;
+            default:
+                break;
+        }
+    }
 
-    const { fields: { idType, idNumber }, handleSubmit, isOpen, riskGroup, validateHasRiskGroup } = this.props;
-    const { selectsReducer, clientR } = this.props;
-    return (
-      <div>
+    _handlerSubmitGroup() {
+        const {fields: {idType, idNumber}, swtShowMessage, findClientByStrTypeIdAndNumber, selectsReducer} = this.props;
+        const strTypeDocument =  _.get(_.find(selectsReducer.get(constants.SHAREHOLDER_ID_TYPE), (item) => _.isEqual(item.id, parseInt(idType.value))),'value','');
+        const jsonFindClient = {
+            strTypeDocument: strTypeDocument,
+            typeDocument: idType.value !== undefined ? idType.value : null,
+            numberDocument: idNumber.value !== undefined ? idNumber.value : null
+        };
+        findClientByStrTypeIdAndNumber(jsonFindClient).then((data) => {
+            if (validateResponse(data)) {
+                this.setState({
+                    showForm: true,
+                    disabledPrimaryFields: true,
+                    clientsBasicInfo: _.get(data, 'payload.data.data', {})
+                });
+            } else {
+                swtShowMessage('error', 'Error consultando el cliente', 'Señor usuario, ocurrió un error tratando de consultar el cliente.');
+            }
+        }, (reason) => {
+            this.setState({showConfirmCreateUser: true});
+        })
+    }
 
-        <div id="content-modal-rosk-group" className="modalBt4-body modal-body business-content editable-form-content clearfix"
-          style={{ overflowX: "hidden" }}>
+    confirmExtClients(confirm) {
+        if (confirm) {
+            this.setState({
+                showConfirmCreateUser: false,
+                disabledPrimaryFields: true,
+                showForm: true,
+                clientsBasicInfo: {}
+            })
+        } else {
+            this.setState({
+                showConfirmCreateUser: false
+            })
+        }
+    }
 
-          <form onSubmit={handleSubmit(this._handlerSubmitGroup)}
-            onKeyPress={val => formValidateKeyEnter(val, true)} style={{ width: "100%" }} >
+    render() {
 
-            <Row style={{ padding: "10px 20px 0px" }}>
+        const {fields: {idType, idNumber}, handleSubmit, isOpen, riskGroup, validateHasRiskGroup} = this.props;
+        const {selectsReducer} = this.props;
+        return (
+            <div>
+                <div id="content-modal-rosk-group"
+                     className="modalBt4-body modal-body business-content editable-form-content clearfix"
+                     style={{overflowX: "hidden"}}>
+                    <form onSubmit={handleSubmit(this._handlerSubmitGroup)}
+                          onKeyPress={val => formValidateKeyEnter(val, true)} style={{width: "100%"}}>
+                        <Row style={{padding: "10px 20px 0px"}}>
+                            <Col xs={12} md={!this.state.disabledPrimaryFields ? 5 : 6}>
+                                <dt><span>Tipo de documento (</span><span style={{color: "red"}}>*</span>)</dt>
+                                <ComboBox
+                                    name="tipoDocumento"
+                                    onChange={val => this._onchangeValue("idType", val)}
+                                    labelInput="Seleccione..."
+                                    {...idType}
+                                    disabled={this.state.disabledPrimaryFields ? "disabled" : ""}
+                                    valueProp={'id'}
+                                    textProp={'value'}
+                                    data={selectsReducer.get(constants.SHAREHOLDER_ID_TYPE)}
+                                />
+                            </Col>
+                            <Col xs={12} md={!this.state.disabledPrimaryFields ? 5 : 6}>
+                                <dt><span>Número de documento (</span><span style={{color: "red"}}>*</span>)</dt>
+                                <Input
+                                    name="documento"
+                                    type="text"
+                                    max="20"
+                                    disabled={this.state.disabledPrimaryFields}
+                                    placeholder="Número de documento"
+                                    {...idNumber}
+                                />
+                            </Col>
+                            {!this.state.disabledPrimaryFields &&
+                            <Col xs={2} md={4} lg={2} style={{paddingTop: '25pt'}}>
+                                <button className="btn btn-primary" type="submit" title="Buscar cliente"
+                                        style={{fontSize: '1.2em'}}>
+                                    <i className="search icon"/>
+                                </button>
+                            </Col>
+                            }
+                            <SweetAlert
+                                type="warning"
+                                show={this.state.showConfirmCreateUser}
+                                showCancelButton={true}
+                                cancelButtonText="Cancelar"
+                                title="Búsqueda de cliente"
+                                text="Señor usuario, el cliente no fue encontrado, ¿desea crearlo?"
+                                onConfirm={() => this.setState({
+                                    showConfirmCreateUser: false,
+                                    disabledPrimaryFields: true,
+                                    showForm: true,
+                                    clientsBasicInfo: {}
+                                })}
+                                onCancel={() => this.setState({showConfirmCreateUser: false})}
+                            />
+                        </Row>
+                    </form >
 
+                    {this.state.showForm &&
+                    <MemberRiskGroup
+                        validateHasRiskGroup={validateHasRiskGroup}
+                        isOpen={isOpen}
+                        riskGroup={riskGroup}
+                        clientsBasicInfo={this.state.clientsBasicInfo}
+                        documentType={idType.value}
+                        documentNumber={idNumber.value}
+                    />
+                    }
+                </div >
 
-              <Col xs={12} md={!this.state.disabledPrimaryFields ? 5 : 6}>
-                <dt><span>Tipo de documento (</span><span style={{ color: "red" }}>*</span>)</dt>
-                <ComboBox
-                  name="tipoDocumento"
-                  onChange={val => this._onchangeValue("idType", val)}
-                  labelInput="Seleccione..."
-                  {...idType}
-                  disabled={this.state.disabledPrimaryFields ? "disabled" : ""}
-                  valueProp={'id'}
-                  textProp={'value'}
-                  data={selectsReducer.get('dataTypeDocument')}
-                />
+                <div className="modalBt4-footer modal-footer">
+                    {this.state.showForm &&
+                    <button className="btn btn-prymary" type="submit"
+                            form={"submitMemberForm"} style={{cursor: 'pointer', marginLeft: "20px"}}>
+                        Agregar </button>
+                    }
+                    <button className="btn btn-default btnDefaultAyax " type="button"
+                            style={{cursor: 'pointer', marginLeft: "20px"}} onClick={() => {
+                        isOpen()
+                    }}>
+                        Cancelar
+                    </button>
+                </div>
+            </div>
 
-
-              </Col>
-              <Col xs={12} md={!this.state.disabledPrimaryFields ? 5 : 6}>
-                <dt><span>Número de documento (</span><span style={{ color: "red" }}>*</span>)</dt>
-                <Input
-                  name="documento"
-                  type="text"
-                  max="20"
-                  disabled={this.state.disabledPrimaryFields}
-                  placeholder="Número de documento"
-                  {...idNumber}
-                />
-              </Col>
-              {!this.state.disabledPrimaryFields &&
-                <Col xs={2} md={4} lg={2} style={{ paddingTop: '25pt' }}>
-                  <button className="btn btn-primary" type="submit" title="Buscar cliente"
-                    style={{ fontSize: '1.2em' }}>
-                    <i className="search icon" ></i>
-                  </button>
-                </Col>
-              }
-              <SweetAlert
-                type="warning"
-                show={this.state.showConfirmCreateUser}
-                showCancelButton={true}
-                cancelButtonText="Cancelar"
-                title="Búsqueda de cliente"
-                text="Señor usuario, el cliente no fue encontrado, ¿desea crearlo?"
-                onConfirm={() => this.setState({
-                  showConfirmCreateUser: false,
-                  disabledPrimaryFields: true,
-                  showForm: true,
-                  clientsBasicInfo: {}
-                })}
-                onCancel={() => this.setState({ showConfirmCreateUser: false })}
-              />
-            </Row>
-          </form >
-
-          {this.state.showForm &&
-            <MemberRiskGroup
-              validateHasRiskGroup={validateHasRiskGroup}
-              isOpen={isOpen}
-              riskGroup={riskGroup}
-              clientsBasicInfo={this.state.clientsBasicInfo}
-              documentType={idType.value}
-              documentNumber={idNumber.value}
-            />
-          }
-        </div >
-
-        <div className="modalBt4-footer modal-footer" >
-          {this.state.showForm &&
-            <button className="btn btn-prymary" type="submit"
-              form={"submitMemberForm"} style={{ cursor: 'pointer', marginLeft: "20px" }}>
-              Agregar </button>
-          }
-          <button className="btn btn-default btnDefaultAyax " type="button"
-            style={{ cursor: 'pointer', marginLeft: "20px" }} onClick={() => { isOpen() }}>
-            Cancelar </button>
-        </div>
-      </div>
-
-    )
-  };
+        )
+    };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    nonValidateEnter,
-    showLoading,
-    swtShowMessage,
-    findClientByTypeAndNumber,
-    consultDataSelect
-  }, dispatch);
+    return bindActionCreators({
+        nonValidateEnter,
+        showLoading,
+        swtShowMessage,
+        findClientByStrTypeIdAndNumber,
+        getMasterDataFields
+    }, dispatch);
 }
 
-function mapStateToProps({ riskGroupReducer, clientInformacion, selectsReducer, clientR }, ownerProps) {
-  return {
-    riskGroupReducer,
-    clientInformacion,
-    selectsReducer,
-    clientR
-  };
+function mapStateToProps({riskGroupReducer, clientInformacion, selectsReducer, clientR}, ownerProps) {
+    return {
+        riskGroupReducer,
+        clientInformacion,
+        selectsReducer,
+        clientR
+    };
 }
 
 export default reduxForm({
-  form: 'submitMember',
-  fields,
-  destroyOnUnmount: true,
-  validate,
-  onSubmitFail: errors => {
-    thisForm.setState({ showErrorForm: true });
-  }
+    form: 'submitMember',
+    fields,
+    destroyOnUnmount: true,
+    validate,
+    onSubmitFail: errors => {
+        thisForm.setState({showErrorForm: true});
+    }
 }, mapStateToProps, mapDispatchToProps)(modalComponentMemberRiskGroup);
