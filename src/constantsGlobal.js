6df317342d5f77ed4b60b8b2dd7dc658d073d482
daloaiza <dloaiza2015@gmail.com>
2018-01-03 08:07:45 -0500

    Merge branch 'release/sprint31_develop' of https://grupobancolombia.visualstudio.com/Vicepresidencia%20Servicios%20de%20Tecnolog%C3%ADa/_git/AW0998001_MS_AYAX_BANCOLOMBIA into release/sprint26_develop
    # Conflicts:
    #	src/constantsGlobal.js

--- src/constantsGlobal.js
+++ src/constantsGlobal.js
@@ -1,4 +1,4 @@
-export const VERSION_DATE = "28/12/2017";
+export const VERSION_DATE = "03/01/2018";
 export const PERSONA_NATURAL = 451;
 export const PERSONA_JURIDICA = 452;
 export const FILE_OPTION_SOCIAL_STYLE_CONTACT = 1;
