d0939c5b26ab957566166360ee7cebdbdc3b303f
lcupitra <lcupitra@bancolombia.com.co>
2018-04-30 10:59:07 -0500

    text area force change

--- src/components/myPendings/linkingRequests/observations/modalObservation.js
+++ src/components/myPendings/linkingRequests/observations/modalObservation.js
@@ -76,7 +76,7 @@ class ModalObservation extends Component {
         const listObservations = linkRequestsReducer.get('observationsByLinkingRequests');
         return (
             <div>
-                <div style={{overflow: 'hidden'}} className="modalBt4-body modal-body business-content editable-form-content clearfix"
+                <div style={{ overflow: 'hidden' }} className="modalBt4-body modal-body business-content editable-form-content clearfix"
                     id="modalCreateBoardMembers">
                     <Row style={{ margin: '15px 0px 0px 10px' }}>
                         <Col xs={6} md={3} lg={3}>
@@ -131,6 +131,7 @@ class ModalObservation extends Component {
                                 title="Ingrese las observaciones"
                                 style={{ width: '100%', height: '108px' }}
                                 max={500}
+                                forceOnChange={true}
                             />
                         </Col>
                     </Row>
