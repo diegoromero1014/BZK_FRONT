9380041389067396b74dbf09988d279f9c8b612d
Cristhian Rios <crarios@bancolombia.com.co>
2018-07-16 16:18:28 -0500

    Reubicación de codigo

--- src/components/pendingTask/actions.js
+++ src/components/pendingTask/actions.js
@@ -2,7 +2,7 @@ import { APP_URL } from '../../constantsGlobal';
 import {
     CLEAR_USER_TASK_CREATE, CLEAR_USER_TASK_ORDER, CLEAR_USER_TASK_CREAR, ORDER_COLUMN_TASK,
     GET_USER_TASK_LIST_CLIENT, CHANGE_PAGE, LIMITE_INF, CLEAR_USER_TASK,
-    CLEAR_USER_TASK_PAGINATOR
+    CLEAR_USER_TASK_PAGINATOR, GET_XLS_TASK
 } from './constants';
 import axios from 'axios';
 
@@ -85,3 +85,30 @@ export function orderColumnUserTask(orderTask, columnTask) {
         columnTask: columnTask
     };
 }
+
+export function getXlsTask(initialDate, finalDate, states) {
+    const json = {
+      "messageHeader": {
+        "sessionToken": window.localStorage.getItem('sessionTokenFront'),
+        "timestamp": new Date().getTime(),
+        "service": "",
+        "status": "0",
+        "language": "es",
+        "displayErrorMessage": "",
+        "technicalErrorMessage": "",
+        "applicationVersion": "",
+        "debug": true,
+        "isSuccessful": true
+      },
+      "messageBody": {
+          "initialDate": initialDate,
+          "finalDate": finalDate,
+          "states": states
+       }
+    }
+    let request = axios.post(APP_URL + "/getXlsTask", json);
+    return {
+      type: GET_XLS_TASK,
+      payload: request
+    }
+  }
