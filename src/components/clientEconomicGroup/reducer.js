7e14f1983f1f16cdd54931e0313b2390a27cdcc6
djposad <djposad@bancolombia.com.co>
2018-08-13 16:53:15 -0500

    grupos economicos front

--- src/components/clientEconomicGroup/reducer.js
+++ src/components/clientEconomicGroup/reducer.js
@@ -10,7 +10,9 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case actions.CONSULT_ECONOMIC_GROUP:
       const response = action.payload.data.data;
-      response.listClients = _.remove(_.get(response, 'listClients', []), client => !_.isEqual(client.document, response.nitEconomicGroup));
+      console.log(response);
+      response.listClients = _.remove(_.get(response, 'listClients', []), client => !_.isEqual(client.id, response.idClientNitPrincipal));
+      console.log(response.listClients);
       return state.withMutations(map => {
         map.set('economicGroupClients', response);
       });
