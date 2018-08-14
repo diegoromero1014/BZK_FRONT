9cf2b2190d731ac35b9335be01396a4a0f893862
djposad <djposad@bancolombia.com.co>
2018-08-14 09:29:47 -0500

    Eliminar console log

--- src/components/clientEconomicGroup/reducer.js
+++ src/components/clientEconomicGroup/reducer.js
@@ -10,9 +10,7 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case actions.CONSULT_ECONOMIC_GROUP:
       const response = action.payload.data.data;
-      console.log(response);
       response.listClients = _.remove(_.get(response, 'listClients', []), client => !_.isEqual(client.id, response.idClientNitPrincipal));
-      console.log(response.listClients);
       return state.withMutations(map => {
         map.set('economicGroupClients', response);
       });
