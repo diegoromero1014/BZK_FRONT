74cae2aa46034e780c67c456404d33b469660d89
djposad <djposad@bancolombia.com.co>
2018-08-27 17:36:04 -0500

    Avance hu gerencia sectorial

--- src/components/clientDetailsInfo/actividadEconomica.js
+++ src/components/clientDetailsInfo/actividadEconomica.js
@@ -69,85 +69,85 @@ class ActividadEconomica extends Component {
                                 <tr>
                                     <td style={{ width: "20%", verticalAlign: "initial" }}>{infoClient.taxNatureKey}</td>
                                     <td style={{ width: "20%", verticalAlign: "initial" }}>{infoClient.valueCustomerTypology}</td>
-                                    <td style={{ width: "20%", verticalAlign: "initial" }}>{infoClient.srtManagementsOfSectorStrategy}</td>
+                                    <td style={{ width: "20%", verticalAlign: "initial" }}>{infoClient.strSectorStrategy}</td>
                                     <td style={{ width: "20%", verticalAlign: "initial" }}></td>
                                 </tr>
                             </tbody>
                         </table>
                     </Col>
                 </Row>
-                
-                { allowAccessContextClient &&
-                    
-                <Row>
-                    <Col xs={12} md={12} lg={12} style={{ textAlign: 'justify', marginTop: '15px' }}>
-                        <dt><span style={{ fontWeight: "bold", color: "#4C5360" }}>Contexto</span></dt>
-                        {_.isNull(contextClient) || _.isUndefined(contextClient) ? "" : contextClient.context}
-                    </Col>
-                </Row>
+
+                {allowAccessContextClient &&
+
+                    <Row>
+                        <Col xs={12} md={12} lg={12} style={{ textAlign: 'justify', marginTop: '15px' }}>
+                            <dt><span style={{ fontWeight: "bold", color: "#4C5360" }}>Contexto</span></dt>
+                            {_.isNull(contextClient) || _.isUndefined(contextClient) ? "" : contextClient.context}
+                        </Col>
+                    </Row>
                 }
-                
-                { allowAccessContextClient &&
+
+                {allowAccessContextClient &&
                     <Row style={{ marginTop: '20px', marginLeft: '1px' }}>
-                    <h3 style={{ width: '100%' }}>Líneas de negocio y participación en ventas</h3>
-                    {!_.isNull(contextClient) && !_.isUndefined(contextClient) && contextClient.noAppliedLineOfBusiness ?
-                        <span>No aplica</span>
-                        :
-                        <div style={{ width: '100%' }}>
-                            {!_.isNull(contextClient) && !_.isUndefined(contextClient) && _.size(contextClient.listParticipation) > 0 ?
-                                <table className='table table-striped' style={{ width: "100%" }}>
-                                    <tr>
-                                        <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Línea de negocio</span></td>
-                                        <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Participación</span></td>
-                                        <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Experiencia (años)</span></td>
-                                        <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Contribución</span></td>
-                                    </tr>
-                                    <tbody>
-                                        {contextClient.listParticipation.map(this._mapValuesParticipation)}
-                                    </tbody>
-                                </table>
-                                :
-                                <Col xs={12} md={12} lg={12}>
-                                    <div style={{ textAlign: "center", marginTop: "20px", marginBottom: "20px" }}>
-                                        <span className="form-item">No se han adicionado líneas de negocio</span>
-                                    </div>
-                                </Col>
-                            }
-                        </div>
-                    }
-                </Row>    
+                        <h3 style={{ width: '100%' }}>Líneas de negocio y participación en ventas</h3>
+                        {!_.isNull(contextClient) && !_.isUndefined(contextClient) && contextClient.noAppliedLineOfBusiness ?
+                            <span>No aplica</span>
+                            :
+                            <div style={{ width: '100%' }}>
+                                {!_.isNull(contextClient) && !_.isUndefined(contextClient) && _.size(contextClient.listParticipation) > 0 ?
+                                    <table className='table table-striped' style={{ width: "100%" }}>
+                                        <tr>
+                                            <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Línea de negocio</span></td>
+                                            <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Participación</span></td>
+                                            <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Experiencia (años)</span></td>
+                                            <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Contribución</span></td>
+                                        </tr>
+                                        <tbody>
+                                            {contextClient.listParticipation.map(this._mapValuesParticipation)}
+                                        </tbody>
+                                    </table>
+                                    :
+                                    <Col xs={12} md={12} lg={12}>
+                                        <div style={{ textAlign: "center", marginTop: "20px", marginBottom: "20px" }}>
+                                            <span className="form-item">No se han adicionado líneas de negocio</span>
+                                        </div>
+                                    </Col>
+                                }
+                            </div>
+                        }
+                    </Row>
                 }
                 {
-                allowAccessContextClient && 
-                <Row style={{ marginTop: '20px', marginLeft: '1px' }}>
-                    <h3 style={{ width: '100%' }}>Canales de distribución y participación en ventas</h3>
-                    {!_.isNull(contextClient) && !_.isUndefined(contextClient) && contextClient.noAppliedDistributionChannel ?
-                        <span>No aplica</span>
-                        :
-                        <div style={{ width: '100%' }}>
-                            {!_.isNull(contextClient) && !_.isUndefined(contextClient) && _.size(contextClient.listDistribution) > 0 ?
-                                <table className='table table-striped' style={{ width: "100%" }}>
-                                    <tr>
-                                        <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Canal de distribución</span></td>
-                                        <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Participación</span></td>
-                                        <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Contribución</span></td>
-                                    </tr>
-                                    <tbody>
-                                        {contextClient.listDistribution.map(this._mapValuesDistribution)}
-                                    </tbody>
-                                </table>
-                                :
-                                <Col xs={12} md={12} lg={12}>
-                                    <div style={{ textAlign: "center", marginTop: "20px", marginBottom: "20px" }}>
-                                        <span className="form-item">No se han adicionado canales de distribución</span>
-                                    </div>
-                                </Col>
-                            }
-                        </div>
-                    }
-                </Row>
+                    allowAccessContextClient &&
+                    <Row style={{ marginTop: '20px', marginLeft: '1px' }}>
+                        <h3 style={{ width: '100%' }}>Canales de distribución y participación en ventas</h3>
+                        {!_.isNull(contextClient) && !_.isUndefined(contextClient) && contextClient.noAppliedDistributionChannel ?
+                            <span>No aplica</span>
+                            :
+                            <div style={{ width: '100%' }}>
+                                {!_.isNull(contextClient) && !_.isUndefined(contextClient) && _.size(contextClient.listDistribution) > 0 ?
+                                    <table className='table table-striped' style={{ width: "100%" }}>
+                                        <tr>
+                                            <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Canal de distribución</span></td>
+                                            <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Participación</span></td>
+                                            <td><span style={{ fontWeight: "bold", color: "#4C5360" }}>Contribución</span></td>
+                                        </tr>
+                                        <tbody>
+                                            {contextClient.listDistribution.map(this._mapValuesDistribution)}
+                                        </tbody>
+                                    </table>
+                                    :
+                                    <Col xs={12} md={12} lg={12}>
+                                        <div style={{ textAlign: "center", marginTop: "20px", marginBottom: "20px" }}>
+                                            <span className="form-item">No se han adicionado canales de distribución</span>
+                                        </div>
+                                    </Col>
+                                }
+                            </div>
+                        }
+                    </Row>
                 }
-                
+
             </div>
         );
     }
