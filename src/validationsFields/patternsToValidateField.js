e24e2da77fd0b393cbd9a782a638d686626d5382
Diego Romero <dromero@ias.com.co>
2018-11-29 12:11:03 -0500

    Revisar reglas de ingreso de texto para formulario miembros de junta FRONT Y SERVICIO Nueva validación - Se agregó la validaćión de no iniciar los valores con @+-= - Se quitaron las vulnerabilidades reportadas por SonarQube en este módulo de miembros de junta.

--- src/validationsFields/patternsToValidateField.js
+++ src/validationsFields/patternsToValidateField.js
@@ -1,10 +1,10 @@
-export const patternOfOnlyAlphabetical = "/[^a-zA-Z\\sÁÉÍÓÚáéíóúÑñÜü]/g";
+export const patternOfOnlyAlphabetical = /^[a-zA-Z\sÁ-Úá-úÑñÜü]+$/;
 export const patternOfOnlyNumbers = "/[^0-9]/g";
-export const patternOfNumberDocument = "/[^a-zA-Z0-9\\-ÁÉÍÓÚáéíóúÑñÜü]/g";
+export const patternOfNumberDocument = /^[a-zA-Z0-9\-Á-Úá-úÑñÜü]+$/;
 export const patternOfPostalCode = "/[^a-zA-Z0-9\\-ÁÉÍÓÚáéíóúÑñÜü]/g";
 export const patternOfAddress = "/[^a-zA-Z0-9\\s\\-ÁÉÍÓÚáéíóúÑñ#();,.\"\/_´']/g";
 export const patternOfNeighborhood = "/[^a-zA-Z0-9\\s\\-ÁÉÍÓÚáéíóúÑñÜü#();,.\"\/]/g";
-export const patternOfObservation = "/[^a-zA-Z0-9\\s()\\[\\];,.\"!¡$%&\/¿?°#=':´+_\\-ÁÉÍÓÚáéíóúÑñÜü]/g";
+export const patternOfObservation = /^[a-zA-Z0-9\s()\[\];,."!¡$%&\/¿?°#=':´+_\-Á-Úá-úÑñÜü]+$/;
 export const patternOfPhone = "/[^0-9]/g";
 export const patternOfContactRelevantFeatures = "/[^a-zA-Z0-9\\s()\\[\\];,.\"!¡$%&\/¿?°#=':´+_\\-ÁÉÍÓÚáéíóúÑñÜü]/g";
 export const patternOfStructureEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
