bb1d157c676047c8ff651b535d6c6641fabdb975
eoltalva <dcasique@bancolombia.com.co>
2019-02-20 17:00:49 -0500

    archivos devops nube

---
+++ scripts/permissions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+exit 0
+# BORRAME: NO HAGO NADA
+#DIR='/IHS/IBM/HTTPServer/biztrack'
+#USER="wassvc"
+#GROUP="wasgrp"
+#rm -rf "$DIR"
+#mkdir "$DIR"
+#chown -R "$USER.$GROUP" "$DIR"
---
+++ scripts/stop_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo -u wassvc /IHS/IBM/HTTPServer/bin/apachectl restart
