be7ab7a86959e0107fd5c14e2599eca60943dee9
Miguel Angel Tovar Freire <matovar@bancolombia.com.co>
2019-02-22 14:46:49 -0500

    Modificacion archivo permissions.sh dejando propietario wassvc

--- deploy/scripts/permissions.sh
+++ deploy/scripts/permissions.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-exit 0
-# BORRAME: NO HAGO NADA
-#DIR='/IHS/IBM/HTTPServer/biztrack'
-#USER="wassvc"
-#GROUP="wasgrp"
-#rm -rf "$DIR"
-#mkdir "$DIR"
-#chown -R "$USER.$GROUP" "$DIR"
+DIR='/IHS/IBM/HTTPServer/biztrack'
+USER="wassvc"
+GROUP="wasgrp"
+chown -R "$USER:$GROUP" "$DIR"
