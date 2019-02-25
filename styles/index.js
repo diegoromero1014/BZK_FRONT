15fadca20301a3bcd77f063dba95e08bfbcebc23
eoltalva <dcasique@bancolombia.com.co>
2019-02-25 17:00:33 -0500

    cambios package e importacion semantic

--- styles/index.js
+++ styles/index.js
@@ -4,7 +4,6 @@ require('./biztrack/modals.scss');
 require('./biztrack/_closeBt4.scss');
 require('./biztrack/tables.scss');
 require('sweetalert/dist/sweetalert.css');
-require('semantic-ui/dist/semantic.css');
 require('../semantic-ui/dist/semantic.css');
 require('react-widgets/lib/less/react-widgets.less');
 require('react-vis/dist/main.scss');
