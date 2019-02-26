a0b870edbe14b9676001337ddd6106fc98728e96
eoltalva <dcasique@bancolombia.com.co>
2019-02-26 09:51:53 -0500

    Se revierte cambio en semantic para no generar afectación. Se debe certificar el cambio

--- styles/index.js
+++ styles/index.js
@@ -4,7 +4,7 @@ require('./biztrack/modals.scss');
 require('./biztrack/_closeBt4.scss');
 require('./biztrack/tables.scss');
 require('sweetalert/dist/sweetalert.css');
-require('../semantic-ui/dist/semantic.css');
+require('semantic-ui/dist/semantic.css');
 require('react-widgets/lib/less/react-widgets.less');
 require('react-vis/dist/main.scss');
 require('./biztrack/scheduler.scss');
