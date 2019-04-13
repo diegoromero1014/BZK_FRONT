aca5ba1d18af511ae41357a6f4bbe066d3691a92
camandgo <camandgo@bancolombia.com.co>
2019-04-12 22:31:34 -0500

    solution to user error

--- src/components/globalComponents/constants.js
+++ src/components/globalComponents/constants.js
@@ -5,3 +5,5 @@ export const ADD_USER = "ADD_USER";
 export const DELETE_USER = "DELETE_USER";
 export const DELETE_PARTICIPANT_VIEW = "DELETE_USER_VIEW";
 export const MESSAGE_CONFIDENTIAL = "Señor usuario acaba de habilitar la opción de confidencial, recuerde que este informe solo podrá ser visto por usted y los usuarios seleccionados.";
+export const TITLE_MESSAGE_ERR_USER_INVALID = "Usuario inválido";
+export const MESSAGE_ERR_USER_INVALID = "Señor usuario, el usuario al que intenta asignarle permisos no es es válido.";
