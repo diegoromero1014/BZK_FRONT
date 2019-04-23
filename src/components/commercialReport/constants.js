76bc0f9a373b61431042e9642232c431134f12a2
camandgo <camandgo@bancolombia.com.co>
2019-04-23 09:53:36 -0500

    create folder commercialReport

---
+++ src/components/commercialReport/constants.js
@@ -0,0 +1,11 @@
+export const ADD_LIST_USER = "ADD_LIST_USER";
+export const CLEAR_USER = "CLEAR_USER";
+export const FILTER_USER = "FILTER_USER";
+export const ADD_USER = "ADD_USER";
+export const DELETE_USER = "DELETE_USER";
+
+export const MESSAGE_CONFIDENTIAL = "Señor usuario acaba de habilitar la opción de confidencial, recuerde que este informe solo podrá ser visto por usted y los usuarios seleccionados.";
+export const TITLE_MESSAGE_ERR_USER_INVALID = "Usuario inválido";
+export const MESSAGE_ERR_USER_INVALID = "Señor usuario, el usuario al que intenta asignarle permisos no es es válido.";
+export const IS_CONFIDENTIAL = "IS_CONFIDENTIAL";
+export const BRAND_CONFIDENTIAL = "Confidential";
