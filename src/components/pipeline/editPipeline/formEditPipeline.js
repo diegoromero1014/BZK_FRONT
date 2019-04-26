b724865d301b25b86f8542400baaeac8bf023ae3
Jerry León Restrepo <jrestrepo@ias.com.co>
2019-04-26 15:08:44 -0500

    valor negativo debe ser en valor del activo proyecto

--- src/components/pipeline/editPipeline/formEditPipeline.js
+++ src/components/pipeline/editPipeline/formEditPipeline.js
@@ -990,7 +990,7 @@ export default function createFormPipeline(name, origin, pipelineBusiness, funct
                                             type="text"
                                             placeholder="Miles ' , ' y decimales ' . '"
                                             parentId="dashboardComponentScroll"
-                                            onBlur={val => handleBlurValueNumber(ALLOWS_NEGATIVE_INTEGER, value, val, true, 2)}
+                                            onBlur={val => handleBlurValueNumber(ONLY_POSITIVE_INTEGER, value, val, true, 2)}
                                             onFocus={val => handleFocusValueNumber(value, value.value)}
                                             disabled={this.state.isEditable && isEditableValue ? '' : 'disabled'}
                                             onChange={val => this._changeValue(val)
@@ -1078,7 +1078,7 @@ export default function createFormPipeline(name, origin, pipelineBusiness, funct
                                             type="text"
                                             {...areaAssetsValue}
                                             parentId="dashboardComponentScroll"
-                                            onBlur={val => handleBlurValueNumber(ONLY_POSITIVE_INTEGER, areaAssetsValue, val, true, 2)}
+                                            onBlur={val => handleBlurValueNumber(ALLOWS_NEGATIVE_INTEGER, areaAssetsValue, val, true, 2)}
                                             onFocus={val => handleFocusValueNumber(areaAssetsValue, areaAssetsValue.value)}
                                             disabled={this.state.isEditable ? '' : 'disabled'}
                                         />
