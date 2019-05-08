84f07faa1a7a9afa641ce883b5c37a279b6b9192
camandgo <camandgo@bancolombia.com.co>
2019-05-08 13:54:00 -0500

    solución error

--- src/components/accordion/componentAccordion.js
+++ src/components/accordion/componentAccordion.js
@@ -12,7 +12,7 @@ class ComponentAccordion extends Component {
         return (
             <div>
                 <div style={{ borderTop: "1px dotted #cea70b", marginTop: "15px" }} ></div>
-                <Accordion defaultActiveIndex={1}>
+                <Accordion defaultActiveIndex={codSection}>
                     <Accordion.Title onClick={functionChange}>
                         <TitleSectionComponent iconClass={icon} fontSize="25px" typeTitle={true}>
                             {title}
