349e4bcda47c9bfb86ffc995b97aa85016c1e227
camandgo <camandgo@bancolombia.com.co>
2019-05-20 15:50:43 -0500

    solución error pruebas

--- test/unit/components/pageUnderConstruction/pageUnderConstruction.spec.js
+++ test/unit/components/pageUnderConstruction/pageUnderConstruction.spec.js
@@ -1,4 +1,4 @@
-import React from './node_modules/react'
+import React from 'react';
 import pageUnderConstruction from '../../../../src/components/pageUnderConstruction/pageUnderConstruction';
 
 describe('Test PageUnderConstruction', () => {
