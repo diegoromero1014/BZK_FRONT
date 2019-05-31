34b67fb228484e0fd81fbaa6ed5f2a5ee21245b7
Edwin Alejandro Oltalvaro Garcia <eoltalva@bancolombia.com.co>
2019-05-31 13:29:13 +0000

    Revert "Merged PR 58827: Revert "Revert "create page under construction""
    Revert !57740
    
    Related work items: #897645"

--- test/unit/components/pageUnderConstruction/pageUnderConstruction.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import pageUnderConstruction from '../../../../src/components/pageUnderConstruction/pageUnderConstruction';
-
-describe('Test PageUnderConstruction', () => {
-  it('should render PageUnderConstruction', () => {
-      const wrapper = shallow(<pageUnderConstruction />);
-      expect(wrapper).to.have.length(1);
-  });
-});
