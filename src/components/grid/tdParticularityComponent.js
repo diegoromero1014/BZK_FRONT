cfcb644c8ac2940c6e5f9b6f5cd536c8a152be56
rcadavi <rcadavi@bancolombia.com.co>
2019-07-02 09:29:54 -0500

    se crea popup de particularidades

--- src/components/grid/tdParticularityComponent.js
+++ src/components/grid/tdParticularityComponent.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import { Popup } from 'semantic-ui-react';
 
+const styleTooltip = {
+  borderRadius: 5,
+  opacity: 0.9
+}
+
 class TdParticularityComponent extends Component {
   render() {      
     const {columnRow, styles} = this.props; 
     return (
-        <td style={styles}>
+        <td style={styles} className="text-center">
             {columnRow.length > 0 ?
-                <Popup trigger={ <button className="btn btn-success btn-sm"><i className={"comment alternate icon"} /></button> } inverted>{columnRow}</Popup>
+                <Popup style={styleTooltip} trigger={ <button className="btn btn-success btn-sm"><i className={"comment alternate icon"} /></button> } inverted position='left center' wide='very'>{columnRow}</Popup>
             : ''}            
         </td>
     );
