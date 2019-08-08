9fac7c0b222b61d90075651232ea71441d362dea
Edwar Jaramillo <edajaram@bancolombia.com.co>
2019-08-08 18:44:28 -0500

    Pruebas unitarias editBusinessPlan/formEdit

--- test/unit/components/businessPlan/editBusinessPlan/formEdit.spec.js
+++ test/unit/components/businessPlan/editBusinessPlan/formEdit.spec.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import Immutable from 'immutable';
-import configureStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import { reducer as formReducer } from "redux-form";
 
-// import ClientTypology from '~/src/components/contextClient/clientTypology';
-import PermissionsUserReports from "~/src/components/commercialReport/permissionsUserReports";
+import PermissionUserReports from "~/src/components/commercialReport/permissionsUserReports";
 import DateTimePickerUi from "~/src/ui/dateTimePicker/dateTimePickerComponent";
 import ComboBox from "~/src/ui/comboBox/comboBoxComponent";
 import RichText from "~/src/components/richText/richTextComponent";
 import NeedBusiness from "~/src/components/businessPlan/need/needBusiness";
 import AreaBusiness from "~/src/components/businessPlan/area/areaBusiness";
 import {FormEdit} from "~/src/components/businessPlan/editBusinessPlan/formEdit.js";
-import PermissionUserReports from "~/src/components/commercialReport/permissionsUserReports";
-// import * as createBusiness from "~/src/components/businessPlan/actions"
-
-// import { nonValidateEnter} from '~/src/actionsGlobal';
-import * as globalActions from '~/src/components/globalComponents/actions';
-
-import * as SelectActions from '~/src/components/selectsComponent/actions';
-import textareaComponent from '../../../../../src/ui/textarea/textareaComponent';
-import { wrap } from 'module';
-
-class ReduxFormField {
-    constructor() {
-        this.onChange = () => { };
-        this.value = '';
-    }
-}
-
 
 const fields = ["initialValidityDate", "finalValidityDate", "objectiveBusiness", "opportunities"];
 const selectsReducer = Immutable.Map({});
@@ -36,7 +15,6 @@ const businessPlanReducer = Immutable.Map({});
 const reducerGlobal = Immutable.Map({});
 const clientInformacion = Immutable.Map({ 'responseClientInfo': { 'key': 'value' } });
 
-const getUserBlockingReport = Immutable.Map({});
 const getMasterDataFields = () => { };
 const nonValidateEnter = () => { };
 const setConfidential = () => { };
