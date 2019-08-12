d93ed397f753907ab28272326bd48ede5a6f06c8
Edwar Jaramillo <edajaram@bancolombia.com.co>
2019-08-12 16:52:16 -0500

    Se adicionan pruebas unitarias para el módulo de plan de negocio

--- test/unit/components/businessPlan/createBusinessPlan/formBusinessPlan.spec.js
+++ test/unit/components/businessPlan/createBusinessPlan/formBusinessPlan.spec.js
@@ -9,7 +9,11 @@ import NeedBusiness from "~/src/components/businessPlan/need/needBusiness";
 import AreaBusiness from "~/src/components/businessPlan/area/areaBusiness";
 import { FormBusinessPlan } from "~/src/components/businessPlan/createBusinessPlan/formBusinessPlan";
 
-const fields = ["initialValidityDate", "finalValidityDate", "objectiveBusiness", "opportunities"];
+const fields = {    "initialValidityDate": { onChange: () => { },
+                                                value: '1567659600000'},
+                    "finalValidityDate": { onChange: () => { },
+                                                value: '1567832400000'},
+                    "objectiveBusiness":"", "opportunities":""};
 const selectsReducer = Immutable.Map({});
 const businessPlanReducer = Immutable.Map({});
 const reducerGlobal = Immutable.Map({});
@@ -19,6 +23,7 @@ const getMasterDataFields = () => { };
 const handleSubmit = () => { };
 const setConfidential = () => { };
 const consultParameterServer = () => { };
+const changeStateSaveData = () => {};
 
 const dataPromise = {payload: { data: { status: 200, validateLogin: true, data: { objective: "", opportunitiesAndThreats: "", initialValidityDate: "",
                             initialValidityDate: "", finalValidityDate: "", clientNeedFulfillmentPlan: "", relatedInternalParties: "", 
@@ -29,7 +34,7 @@ const resolveDataCreate = () => {
     return new Promise( (resolve, reject) => resolve(dataPromise) )
 }
 
-const defaultProps = { fields: fields, selectsReducer, handleSubmit, businessPlanReducer, reducerGlobal,
+const defaultProps = { fields: fields, selectsReducer, handleSubmit, businessPlanReducer, reducerGlobal, changeStateSaveData,
                         setConfidential, clientInformacion, getMasterDataFields, consultParameterServer };
 
 describe('Test BusinessPlan/createBusinessPlan/formBusinessPlan', () => {
@@ -44,23 +49,31 @@ describe('Test BusinessPlan/createBusinessPlan/formBusinessPlan', () => {
         expect(wrapper.find(DateTimePickerUi)).to.have.length(2);
     });
 
-    it('should render DateTimePickerUi', () => {
+    it('should render ComboBox', () => {
         const wrapper = shallow(<FormBusinessPlan {...defaultProps} consultParameterServer={resolveDataCreate} />);
         expect(wrapper.find(ComboBox)).to.have.length(1);
     });
 
-    it('should render DateTimePickerUi', () => {
+    it('should render RichText', () => {
         const wrapper = shallow(<FormBusinessPlan {...defaultProps} consultParameterServer={resolveDataCreate} />);
         expect(wrapper.find(RichText)).to.have.length(1);
     });
 
-    it('should render DateTimePickerUi', () => {
+    it('should render NeedBusiness', () => {
         const wrapper = shallow(<FormBusinessPlan {...defaultProps} consultParameterServer={resolveDataCreate} />);
         expect(wrapper.find(NeedBusiness)).to.have.length(1);
     });
 
-    it('should render DateTimePickerUi', () => {
+    it('should render AreaBusiness', () => {
         const wrapper = shallow(<FormBusinessPlan {...defaultProps} consultParameterServer={resolveDataCreate} />);
         expect(wrapper.find(AreaBusiness)).to.have.length(1);
+    });
+    
+    it('should be ok when initialValidityDate is smaller or equals to finalValidityDate', () => {
+        const wrapper = shallow(<FormBusinessPlan {...defaultProps} consultParameterServer={resolveDataCreate} createBusiness={resolveDataCreate}
+                                initialValidityDate= "08/08/2019" finalValidityDate="09/08/2019" validateRangeDates={resolveDataCreate} />);
+
+        wrapper.instance()._onSelectFieldDate("08/08/2019", "09/08/2019", "09/08/2019", true);
+        expect(wrapper.state().initialDateError).equal(false);
     });
 });
