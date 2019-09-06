7f28d82f3503d131097ba7b14fb7a763466f1420
Edwin Alejandro Oltalvaro Garcia <eoltalva@bancolombia.com.co>
2019-09-06 13:23:39 -0500

    restablecer validez del token al loguearse

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -7,7 +7,7 @@ export const invalidTokenMiddleware = store => next => action => {
     const status = _.get(action, "payload.data.status", 200);
     const validateLogin = _.get(action, "payload.data.validateLogin", true);
     const message = _.get(action, "payload.data.data", "");
-    if ((status == 500 && !validateLogin) || message == "Token is not longer valid") {
+    if ((status == 500 && !validateLogin) || message === "Token is not longer valid") {
         let newAction = {
             type: VALID_TOKEN,
             value: false
