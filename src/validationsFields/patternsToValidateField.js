566122b9727d7d34f53f6a1a6fab2c25cbb5fc3d
Daniel Alejandro Rodriguez Gomez <daalrodr@bancolombia.com.co>
2019-12-16 11:23:47 -0500

    Cierre vulnerabilidad FIN.S.0063. Falta de validación de datos y FIN.S.0045. Inyección de código (HTML) Agregar Cliente -Grupo Riesgo FRONT

--- src/validationsFields/patternsToValidateField.js
+++ src/validationsFields/patternsToValidateField.js
@@ -37,6 +37,6 @@ export const patternOfNoOperatingInCome = /^[a-zA-Z0-9ñÑá-úÁ-Ú\s();,.\-"!$
 export const patternOfOnlyAlphabeticalAndSlash = /^[a-zA-Z\sÁ-Úá-úÑñÜü\\\]/]+$/;
 export const patternOfRiskGroupName = /^[a-zA-Z0-9ñÑá-úÁ-Ú\s&/\\,;.#"°()'\-+´:]+$/;
 export const patternOfObservationRiskGroup = /^[a-zA-Z0-9ñÑá-úÁ-Ú\s();,.\-"!$%&/¿?°#=¡':´+\[\]_]+$/;
-export const patternOfJustificationsRiskGroup = /^[a-zA-Z0-9ñÑá-úÁ-Ú\s();,.\-"!$%&/¿?°#=¡':´+\[\]_]+$/;
+export const patternOfJustificationsRiskGroup = /^[a-zA-Z0-9ñÑá-úÁ-Ú\s();,.\-"!$%&/¿?°#=¡':´+@*\<\>\[\]_]+$/;
 export const patternOfExternalClientNumberDocument = /^[a-zA-Z0-9\-Á-Úá-úÑñÜü]+$/;
 export const regexHtmlInjection = /(.*(\<|&lt;|&lt)\s*\/.*(\>|&gt|&gt;))|(.*(\<|&lt;|&lt).*\/\s*(\>|&gt|&gt;))/;
