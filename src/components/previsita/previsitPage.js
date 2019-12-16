7860d831456556f909b81b22ba90a0c0ce795882
Daniel Esteban Gallego Sierra <daegalle@bancolombia.com.co>
2019-12-16 13:33:12 -0500

    Incorporación del componente de confidencialidad en el componente padre

--- src/components/previsita/previsitPage.js
+++ src/components/previsita/previsitPage.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { redirectUrl } from '../globalComponents/actions';
-import { ComponentClientInformationURL } from '../../constantsAnalytics';
 import HeaderPrevisita from './headerPrevisita';
 import PrevisitFormComponent from './previsitFormComponent'
-import { detailPrevisit, canEditPrevisita, disableBlockedReport, clearPrevisitDetail } from "./actions";
+import { detailPrevisit, canEditPrevisita, disableBlockedReport, clearPrevisitDetail, validateDatePreVisit, createPrevisit } from "./actions";
 import { showLoading } from '../loading/actions';
-import { TIME_REQUEST_BLOCK_REPORT, MESSAGE_ERROR, MESSAGE_ERROR_SWEET_ALERT, EDITAR } from '../../constantsGlobal';
+import { TIME_REQUEST_BLOCK_REPORT, MESSAGE_ERROR, MESSAGE_ERROR_SWEET_ALERT, EDITAR, REQUEST_ERROR, MESSAGE_SAVE_DATA, REQUEST_INVALID_INPUT, REQUEST_SUCCESS } from '../../constantsGlobal';
 import { swtShowMessage } from '../sweetAlertMessages/actions';
 import { Row, Col } from 'react-flexbox-grid';
 import { PREVISIT_TYPE } from '../selectsComponent/constants';
 import { getMasterDataFields } from '../selectsComponent/actions';
+import { redirectUrl } from '../globalComponents/actions';
+import { PermissionUserReports } from '../commercialReport/permissionsUserReports';
+import { buildJsoncommercialReport } from '../commercialReport/functionsGenerics';
+import moment from 'moment';
 
 export class PrevisitPage extends Component {
 
-   constructor(props){
-      super(props);      
+   constructor(props) {
+      super(props);
    }
 
    state = {
@@ -31,24 +33,25 @@ export class PrevisitPage extends Component {
       previsitTypes: []
    };
 
-   async componentWillMount() {
-      const { params: { id }, clientInformacion } = this.props;
+   componentWillMount() {
+      const { clientInformacion } = this.props;
       const infoClient = clientInformacion.get('responseClientInfo');
-/* 
-      if (_.isEmpty(infoClient)) {
-         redirectUrl(ComponentClientInformationURL)
-      } */
-
-      await this.getPrevisitTypes();
-      await this.getPrevisitData(id);  
-      this.setState({
-         renderForm: true
-      });          
+      /* 
+            if (_.isEmpty(infoClient)) {
+               redirectUrl(ComponentClientInformationURL)
+            } */
    }
 
    componentDidMount() {
-      this.setState({
-         isMounted: true
+      const { params: { id }, dispatchShowLoading } = this.props;
+      dispatchShowLoading(true, "Cargando...");
+
+      Promise.all([this.getPrevisitTypes(), this.getPrevisitData(id)]).then(() => {
+         this.setState({
+            renderForm: true,
+            isMounted: true
+         });
+         dispatchShowLoading(false, "");
       });
    }
 
@@ -61,62 +64,57 @@ export class PrevisitPage extends Component {
          isMounted: false
       });
 
-      if (this.state.isEditable) {         
-         dispatchDisabledBlockedReport(id);         
+      if (this.state.isEditable) {
+         dispatchDisabledBlockedReport(id);
       }
       dispatchClearPrevisitDetail();
    }
 
-   getPrevisitData = async (id)=> {
+   getPrevisitData = async (id) => {
       const { dispatchDetailPrevisit } = this.props;
       if (id) {
-         showLoading(true, "Cargando...");
-         await dispatchDetailPrevisit(id); 
-         showLoading(false, null);
+         await dispatchDetailPrevisit(id);
          this.setState({
-            isEditable: true      
+            isEditable: true
          });
-      }else{
+      } else {
          this.setState({
-            isEditable: false           
+            isEditable: false
          });
-      }      
+      }
    }
 
    getPrevisitTypes = async () => {
-      const {dispatchGetMasterDataFields, selectsReducer} = this.props;
+      const { dispatchGetMasterDataFields } = this.props;
       await dispatchGetMasterDataFields([PREVISIT_TYPE]);
-      this.setState({
-         previsitTypes: selectsReducer.get(PREVISIT_TYPE) || []
-      });
    }
 
    validatePermissionsPrevisits = () => {
-      const { reducerGlobal } = this.props;      
+      const { reducerGlobal } = this.props;
       return _.get(reducerGlobal.get('permissionsPrevisits'), _.indexOf(reducerGlobal.get('permissionsPrevisits'), EDITAR), false) && this.state.isEditable;
    }
 
-   canUserEditPrevisita = (myUserName) => {          
-      const { dispatchCanEditPrevisita, params: { id }, dispatchSwtShowMessage } = this.props;
+   canUserEditPrevisita = (myUserName) => {
+      const { dispatchCanEditPrevisita, params: { id }, dispatchSwtShowMessage, dispatchShowLoading } = this.props;
       dispatchCanEditPrevisita(id).then((success) => {
          const username = success.payload.data.data.username
          const name = success.payload.data.data.name
 
          if (!this.state.isMounted) {
-            clearInterval(this.state.intervalId);            
+            clearInterval(this.state.intervalId);
          }
 
          if (success.payload.data.data == null) {
             clearInterval(this.state.intervalId);
-            this.setState({ 
-               showErrorBlockedPreVisit: true, 
-               userEditingPrevisita: "Error", 
-               shouldRedirect: true 
-            });            
+            this.setState({
+               showErrorBlockedPreVisit: true,
+               userEditingPrevisita: "Error",
+               shouldRedirect: true
+            });
          }
 
-         if (_.isNull(username)) {            
-            dispatchSwtShowMessage(MESSAGE_ERROR, MESSAGE_ERROR_SWEET_ALERT);            
+         if (_.isNull(username)) {
+            dispatchSwtShowMessage(MESSAGE_ERROR, MESSAGE_ERROR_SWEET_ALERT);
          } else if (username.toUpperCase() === myUserName.toUpperCase()) {
             // Usuario pidiendo permiso es el mismo que esta bloqueando
             if (this.state.isEditable) {
@@ -146,27 +144,98 @@ export class PrevisitPage extends Component {
             } else {
                // Mostar mensaje de el usuario que tiene bloqueado el informe
                this.setState({ showErrorBlockedPreVisit: true, userEditingPrevisita: name, shouldRedirect: false })
-            }            
+            }
          }
-         showLoading(false, null);
+         dispatchShowLoading(false, "");
       });
    }
 
-   editPrevisit = () => {            
-      showLoading(true, "Cargando...");
-      const usernameSession = window.localStorage.getItem('userNameFront');      
-      this.canUserEditPrevisita(usernameSession);      
+   editPrevisit = () => {
+      const { dispatchShowLoading } = this.props;
+      dispatchShowLoading(true, "Cargando...");
+      const usernameSession = window.localStorage.getItem('userNameFront');
+      this.canUserEditPrevisita(usernameSession);
+      dispatchShowLoading(false, "");
    }
 
-   submitForm = (data) => {
+   submitForm = async (previsit, isDraft) => {
+      const { dispatchShowLoading, dispatchCreatePrevisit, usersPermission, confidentialReducer } = this.props;
+      const validateDatePrevisitResponse = await this.validateDatePrevisit(previsit);      
 
+      try {
+         if (validateDatePrevisitResponse) {
+            const previsitRequest = {
+               "id": null,
+               "client": window.sessionStorage.getItem('idClientSelected'),
+               "visitTime": parseInt(moment(previsit.datePreVisit).format('x')),
+               "participatingContacts": previsit.dataClient.length === 0 ? null : dataClient,
+               "participatingEmployees": previsit.dataBanco.length === 0 ? null : dataBanco,
+               "relatedEmployees": previsit.dataOthers.length === 0 ? null : dataOthers,
+               "principalObjective": previsit.targetPrevisit,
+               "documentType": previsit.typePreVisit,
+               "visitLocation": previsit.lugarPrevisit,
+               "observations": previsit.pendingPrevisit,
+               "clientTeach": previsit.clientTeach,
+               "adaptMessage": previsit.adaptMessage,
+               "controlConversation": previsit.controlConversation,
+               "constructiveTension": previsit.constructiveTension,
+               "documentStatus": typeButtonClick,
+               "endTime": previsit.durationPreVisit,
+               "commercialReport": buildJsoncommercialReport(null, usersPermission.toArray(), confidentialReducer.get('confidential'), isDraft)
+            };            
+            dispatchShowLoading(true, MESSAGE_SAVE_DATA);
+            const responseCreatePrevisit = await dispatchCreatePrevisit(previsitRequest);
+            dispatchShowLoading(false, "");
+            if (!_.get(responseCreatePrevisit, 'payload.data.validateLogin') || _.get(responseCreatePrevisit, 'payload.data.validateLogin') === 'false') {
+               redirectUrl("/login");
+            } else if (_.get(responseCreatePrevisit, 'payload.data.status') === REQUEST_SUCCESS) {
+               swtShowMessage('success', "Creación previsita", "Señor usuario, la previsita se creó de forma exitosa.", { onConfirmCallback: redirectUrl("/dashboard/clientInformation") });
+            } else if (_.get(responseCreatePrevisit, 'payload.data.status') === REQUEST_INVALID_INPUT) {
+               swtShowMessage('error', "Creación previsita", "Señor usuario, los datos enviados contienen caracteres invalidos que deben ser corregidos.");
+            } else {
+               swtShowMessage('error', "Creación previsita", "Señor usuario, ocurrió un error creando la previsita.");
+            }
+         }
+      } catch (error) {
+         swtShowMessage('error', "Creación previsita", "Señor usuario, ocurrió un error creando la previsita.");
+      }
+   }
+
+   validateDatePrevisit = async (previsit) => {
+      const { dispatchValidateDatePrevisit, dispatchSwtShowMessage } = this.props;
+      let visitTime = parseInt(moment(previsit.visitTime).startOf('minute').format('x'));
+      let endVisitTime = parseInt(moment(previsit.visitTime).add(previsit.endTime, 'h').startOf('minute').format('x'));
+      const response = await dispatchValidateDatePrevisit(visitTime, endVisitTime);
+      if (response.status == REQUEST_ERROR) {
+         dispatchSwtShowMessage(MESSAGE_ERROR, 'Vigencia de fechas', response.data);
+         return false;
+      }
+      return true;
+   }
+
+   renderForm = () => {
+      const { previsitReducer, selectsReducer } = this.props;
+      return (
+         <div>            
+            <PrevisitFormComponent
+               previsitData={previsitReducer.get('detailPrevisit') ? previsitReducer.get('detailPrevisit').data : null}
+               previsitTypes={selectsReducer.get(PREVISIT_TYPE)}
+               isEditable={this.state.isEditable}
+               onSubmit={this.submitForm} />
+         </div>);
    }
 
    render() {
-      const { previsitReducer } = this.props;
       return (
-         <div>            
+         <div>
             <HeaderPrevisita />
+            <div>
+               <Row>
+                  <Col xs={12} md={12} lg={12}>
+                     <PermissionUserReports />
+                  </Col>
+               </Row>
+            </div>
             <div style={{ backgroundColor: "#FFFFFF", paddingTop: "10px", width: "100%", paddingBottom: "50px" }}>
                <Row style={{ padding: "5px 10px 0px 20px" }}>
                   <Col xs={10} sm={10} md={10} lg={10}>
@@ -182,17 +251,11 @@ export class PrevisitPage extends Component {
                         </button>
                      }
                   </Col>
-               </Row>
+               </Row>               
             </div>            
             {
-               this.state.renderForm && previsitReducer ?
-                  <PrevisitFormComponent 
-                     previsitData={previsitReducer.get('detailPrevisit') ? previsitReducer.get('detailPrevisit').data : null} 
-                     previsitTypes={this.state.previsitTypes}
-                     isEditable={this.state.isEditable} 
-                     onSubmit={this.submitForm}/>
-               : null
-            }            
+               this.state.renderForm ? this.renderForm() : null
+            }
          </div>
       )
    }
@@ -206,16 +269,20 @@ function mapDispatchToProps(dispatch) {
       dispatchSwtShowMessage: swtShowMessage,
       dispatchDisabledBlockedReport: disableBlockedReport,
       dispatchClearPrevisitDetail: clearPrevisitDetail,
-      dispatchGetMasterDataFields: getMasterDataFields
+      dispatchGetMasterDataFields: getMasterDataFields,
+      dispatchValidateDatePrevisit: validateDatePreVisit,
+      dispatchCreatePrevisit: createPrevisit
    }, dispatch);
 }
 
-function mapStateToProps({ clientInformacion, reducerGlobal, previsitReducer, selectsReducer }) {
+function mapStateToProps({ clientInformacion, reducerGlobal, previsitReducer, selectsReducer, usersPermission, confidentialReducer }) {
    return {
       clientInformacion,
       previsitReducer,
       reducerGlobal,
-      selectsReducer
+      selectsReducer,
+      usersPermission,
+      confidentialReducer
    };
 }
 
