10f6c1b70b738938bfc4999b0414826b2a551503
Edwin Alejandro Oltalvaro Garcia <eoltalva@bancolombia.com.co>
2020-01-16 13:26:36 -0500

    pruebas unitarias createClientDetailRequestFromReducer

--- test/unit/components/fieldList/mapListsToEntities.spec.js
+++ test/unit/components/fieldList/mapListsToEntities.spec.js
@@ -1,4 +1,7 @@
-import { clientInformationToReducer } from '../../../../src/components/fieldList/mapListsToEntities'
+import {
+    clientInformationToReducer,
+    createClientDetailRequestFromReducer
+} from '../../../../src/components/fieldList/mapListsToEntities'
 
 describe("Test ClientInformationToReducer", () => {
 
@@ -61,4 +64,137 @@ describe("Test ClientInformationToReducer", () => {
         expect(result[0].strategies[0]["fieldlist-id"]).to.equal(2);
     })
 
-})
+});
+
+describe("Test createClientDetailRequestFromReducer", () => {
+
+    it("createClientDetailRequestFromReducer", () => {
+
+        const fieldListReducer = {
+            objectives: {
+                elements: []
+            }
+        }
+    
+        const objectListReducer = {
+            Oportunidades: {
+                elements: []
+            },
+            Debilidades: {
+                elements: []
+            }
+        }
+    
+        const clientId = 1;
+
+        const result = createClientDetailRequestFromReducer(fieldListReducer, objectListReducer, clientId);
+
+        expect(result.objectives.length).to.equal(0);
+        expect(result.opportunities.length).to.equal(0);
+        expect(result.weaknesses.length).to.equal(0);
+    });
+
+
+    it("should return objectives", () => {
+        const fieldListReducer = {
+            objectives: {
+                elements: [
+                    {
+                        value: "objetivo 1"
+                    },
+                    {
+                        value: "objetivo 2"
+                    }
+                ]
+            }
+        }
+    
+        const objectListReducer = {
+            Oportunidades: {
+                elements: []
+            },
+            Debilidades: {
+                elements: []
+            }
+        }
+    
+        const clientId = 1;
+
+        const result = createClientDetailRequestFromReducer(fieldListReducer, objectListReducer, clientId);
+
+        expect(result.objectives.length).to.equal(2);
+        expect(result.objectives[0].text).to.equal("objetivo 1");
+        expect(result.objectives[1].text).to.equal("objetivo 2");
+        expect(result.opportunities.length).to.equal(0);
+        expect(result.weaknesses.length).to.equal(0);
+    });
+
+    it("should return strategies in objective", () => {
+        const fieldListReducer = {
+            objectives: {
+                elements: [
+                    {
+                        value: "objetivo 1",
+                        strategies: [
+                            {
+                                value: "estrategia"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    
+        const objectListReducer = {
+            Oportunidades: {
+                elements: []
+            },
+            Debilidades: {
+                elements: []
+            }
+        }
+    
+        const clientId = 1;
+
+        const result = createClientDetailRequestFromReducer(fieldListReducer, objectListReducer, clientId);
+
+        expect(result.objectives.length).to.equal(1);
+        expect(result.objectives[0].relations.length).to.equal(1);
+        expect(result.objectives[0].relations[0].clientDetailRelation.text).to.equal("estrategia");
+        expect(result.opportunities.length).to.equal(0);
+        expect(result.weaknesses.length).to.equal(0);
+    });
+
+
+    it("should return opportunities", () => {
+        const fieldListReducer = {
+            objectives: {
+                elements: [
+                ]
+            }
+        }
+    
+        const objectListReducer = {
+            Oportunidades: {
+                elements: [
+                    {
+                        text: "oportunidad"
+                    }
+                ]
+            },
+            Debilidades: {
+                elements: []
+            }
+        }
+    
+        const clientId = 1;
+
+        const result = createClientDetailRequestFromReducer(fieldListReducer, objectListReducer, clientId);
+
+        expect(result.objectives.length).to.equal(0);
+        expect(result.opportunities.length).to.equal(1);
+        expect(result.opportunities[0].text).to.equal("oportunidad");
+        expect(result.weaknesses.length).to.equal(0);
+    });
+
+});
