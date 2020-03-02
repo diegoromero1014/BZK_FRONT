f5b2e38af8a311f42d782eb15c511a0950cdd895
Edwin Otalvaro <ealejandro.otalvaro@gmail.com>
2020-03-02 08:12:32 -0500

    pruebas unitarias

--- test/unit/components/fieldList/Objectives/AssociateObjectives.spec.js
+++ test/unit/components/fieldList/Objectives/AssociateObjectives.spec.js
@@ -1,4 +1,5 @@
 import AssociateObjectives from '../../../../../src/components/fieldList/Objetives/AssociateObjectives';
+import BiztrackModal from '../../../../../src/components/fieldList/Objetives/BiztrackModal'
 
 describe("Test AssociateObjectives", () => {
 
@@ -41,5 +42,78 @@ describe("Test AssociateObjectives", () => {
         expect(changeListState.callCount).to.equals(1);
         expect(changeListState.firstCall.lastArg.draftElements).to.have.length(elements.length)
         expect(changeListState.firstCall.lastArg.showAssociateSection).to.equals(true)
+    })
+
+    it("should show BiztrackModal when showAssociateSection is true", () => {
+        const wrapper = shallow(<AssociateObjectives 
+            {...defaultProps}
+            showAssociateSection={true}
+        />);
+
+        expect(wrapper.find(BiztrackModal)).to.have.length(1);
+        
+    });
+
+    it("should changeAssociationOfElements", () => {
+
+        const elements = [
+            {
+                id: 1,
+                associated: false,
+                strategies: [{
+                    id: 2,
+                    associated: false
+                }]
+            }
+        ]
+
+        const changedElement = {
+            id: 1
+        }
+
+        const wrapper = shallow(<AssociateObjectives {...defaultProps} />)
+        const result = wrapper.instance().changeAssociationOfElements(elements, changedElement);
+
+        expect(result).to.have.length(1);
+        expect(result[0].associated).to.equal(true);
+        expect(result[0].strategies[0].associated).to.equal(true)
+
+    })
+
+    it("should show error message when there is not checked element", () => {
+        const swtShowMessage = sinon.fake();
+        const draftElements = [
+            {
+                id: 5,
+                associated: false
+            }
+        ]
+        const wrapper = shallow(<AssociateObjectives
+            {...defaultProps}
+            swtShowMessage={swtShowMessage}
+            draftElements={draftElements}
+        />)
+        wrapper.instance().associateElements();
+        expect(swtShowMessage.callCount).to.equal(1);
+    })
+
+    it("should changeListState when there is checked element", () => {
+        const swtShowMessage = sinon.fake();
+        const changeListState = sinon.fake();
+        const draftElements = [
+            {
+                id: 5,
+                associated: true
+            }
+        ]
+        const wrapper = shallow(<AssociateObjectives
+            {...defaultProps}
+            swtShowMessage={swtShowMessage}
+            draftElements={draftElements}
+            changeListState={changeListState}
+        />)
+        wrapper.instance().associateElements();
+        expect(swtShowMessage.callCount).to.equal(0);
+        expect(changeListState.callCount).to.equal(1)
     })
 })
