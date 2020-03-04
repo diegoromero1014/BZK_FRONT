3db98af9fc0803d4d0d42bfe5517a4366ed1e50b
Cristhian <crarios@bancolombia.com.co>
2020-03-04 09:06:35 -0500

    se agrega validación longitud en los campos de tareas

--- src/components/pendingTask/taskSchema.js
+++ src/components/pendingTask/taskSchema.js
@@ -8,7 +8,7 @@ import {
     MESSAGE_ERROR_INJECTION_HTML,
     MESSAGE_REQUIRED_EMPLOYEE,
     MESSAGE_REQUIRED_FIELD,
-    MESSAGE_WARNING_FORBIDDEN_CHARACTER,
+    MESSAGE_WARNING_FORBIDDEN_CHARACTER, MESSAGE_WARNING_MAX_LENGTH,
     MESSAGE_WARNING_ONLY_ALPHABETICAL,
     MESSAGE_WARNING_TASK_OBSERVATIONS
 } from '../../validationsFields/validationsMessages';
@@ -27,6 +27,7 @@ export const schema = {
         .trim()
         .required(MESSAGE_REQUIRED_EMPLOYEE)
         .matches(patternOfOnlyAlphabetical, MESSAGE_WARNING_ONLY_ALPHABETICAL)
+        .max(50, MESSAGE_WARNING_MAX_LENGTH(50))
         .typeError(MESSAGE_REQUIRED_EMPLOYEE),
     task: Yup.string()
         .trim()
@@ -36,5 +37,6 @@ export const schema = {
         .matches(patternOfTaskObservation, MESSAGE_WARNING_TASK_OBSERVATIONS)
         .matches(patternOfForbiddenCharacter2, {message: MESSAGE_WARNING_FORBIDDEN_CHARACTER, excludeEmptyString: true})
         .test('validateInjectionHtml', MESSAGE_ERROR_INJECTION_HTML, validateHtmlInjection)
+        .max(1000, MESSAGE_WARNING_MAX_LENGTH(1000))
         .nullable()
 };
