fddf5416ef49e28fb3b008b8c1a5d09d37d4127e
Daniel Alejandro Rodriguez Gomez <daalrodr@bancolombia.com.co>
2020-03-06 09:54:29 -0500

    Mejora pruebas unitarias

--- test/unit/ui/comboBoxFilter/trafficLightIndicator.spec.js
+++ test/unit/ui/comboBoxFilter/trafficLightIndicator.spec.js
@@ -1,7 +1,12 @@
 import React from "react";
 import TrafficLightIndicator from './../../../../src/ui/TrafficLightIndicator';
 import { shallow } from "enzyme";
-
+import TrafficLightComponent from './../../../../src/components/grid/trafficLightComponent';
+import {
+  COLOR_RED,
+  COLOR_ORANGE,
+  COLOR_GREEN
+} from './../../../../src/components/clientInformation/constants';
 
 describe("Test TrafficLightIndicator",  () => {
     it("should render green traffic light", () => {
@@ -9,28 +14,32 @@ describe("Test TrafficLightIndicator",  () => {
             days:7,
             isFinalized: false
         }
-        const wrapper = shallow(<TrafficLightIndicator {...props}></TrafficLightIndicator>);
+        const wrapper = shallow(<TrafficLightIndicator {...props}></TrafficLightIndicator>).find(TrafficLightComponent);
         expect(wrapper).to.have.length(1);
+        expect(wrapper.props().colorTraffict.color).to.equals(COLOR_GREEN);
     })
-    it("should render yellow traffic light", () => {
-      const props = {
-        days: 3,
-        isFinalized: false
-      };
-      const wrapper = shallow(
-        <TrafficLightIndicator {...props}></TrafficLightIndicator>
-      );
-      expect(wrapper).to.have.length(1);
+    it("should render orange traffic light", () => {
+        const props = {
+            days: 3,
+            isFinalized: false
+        };
+        const wrapper = shallow(
+            <TrafficLightIndicator {...props}></TrafficLightIndicator>
+        );
+        expect(wrapper).to.have.length(1);            
+        expect(wrapper.props().colorTraffict.color).to.equals(COLOR_ORANGE);
+
     });
     it("should render red traffic light", () => {
-      const props = {
-        days: 0,
-        isFinalized: false
-      };
-      const wrapper = shallow(
-        <TrafficLightIndicator {...props}></TrafficLightIndicator>
-      );
-      expect(wrapper).to.have.length(1);
+        const props = {
+            days: 0,
+            isFinalized: false
+        };
+        const wrapper = shallow(
+            <TrafficLightIndicator {...props}></TrafficLightIndicator>
+        );
+        expect(wrapper).to.have.length(1);
+        expect(wrapper.props().colorTraffict.color).to.equals(COLOR_RED);
     });
     it("should not render traffic light", () => {
       const props = {
@@ -39,7 +48,7 @@ describe("Test TrafficLightIndicator",  () => {
       };
       const wrapper = shallow(
         <TrafficLightIndicator {...props}></TrafficLightIndicator>
-      );
-      expect(wrapper).to.have.length(1);
+      );      
+      expect(Object.keys(wrapper).length).to.equals(0);
     });
 });
