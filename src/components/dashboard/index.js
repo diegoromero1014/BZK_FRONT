3e524f442c08d7203e13f463ce24166cc5108481
Alejandro García <jualcarv@bancolombia.com.co>
2020-03-09 18:46:29 -0500

    Avance pruebas unitarias.

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -4,8 +4,8 @@ import { bindActionCreators } from 'redux';
 import { Row, Col } from 'react-flexbox-grid';
 
 import Reports from './widgets/reports';
+import Header from './header';
 
-import { getUsername } from './actions';
 import { updateTitleNavBar } from '../navBar/actions';
 
 import { MESSAGE_CONFIDENTIAL } from './constants';
@@ -23,28 +23,13 @@ export class Dashboard extends Component {
     dispatchUpdateTitleNavBar("Dashboard");
   }
 
-  formatName = () => {
-    const name = getUsername().toLowerCase();
-    return name.split(' ')[0];
-  }
-
   render() {
-
     return (
         <div className="ui segment" style={{ paddingLeft: 50, paddingRigth: 50, height: '100%' }}>  
             <div style={{ color: "#7f7f7f" }}>
                 <span style={{ fontStyle: "italic" }}>{MESSAGE_CONFIDENTIAL}</span>
             </div>
-            <Row>
-                <Col md={3}>
-                    <div style={{ marginTop: 30, fontSize: 20, textTransform: 'capitalize' }}>
-                        Bienvenid@ {this.formatName()}
-                    </div>
-                </Col>
-                <Col mdOffset={8} md={1} >
-                    <img src={'../../../img/icon/settings.png'} alt={'title'} style={{ fontSize: 40, marginLeft: '50px', marginTop: 30, cursor: 'pointer' }} />
-                </Col>
-            </Row>
+            <Header />
             <Row>
                 <Col md={12} style={{ marginTop: 50 }}>
                     <Reports />
---
+++ test/unit/components/dashboard/header/indexTest.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../../../../../src/components/dashboard/header';
+
+
+describe('Header Test', () => {
+
+    it('Should render component', () => {
+        itRenders(<Header />);
+    })
+})
