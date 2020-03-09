9ddf0aa1b8b006864ce9c655165fdc59b9e3ed99
camandgo <camandgo@bancolombia.com.co>
2020-03-09 14:51:34 -0500

    Avance

--- src/components/dashboard/widgets/reports/ReportCardView.js
+++ src/components/dashboard/widgets/reports/ReportCardView.js
@@ -5,15 +5,16 @@ import { Icon } from 'semantic-ui-react'
 const ReportCardView = (props) => {
     return (
         <div className="report-card-view" {...props}>
-            <img src={props.background} alt={props.title} style={{ width: '100%', height: '100%', zIndex: -1 }} />
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', zIndex: -1, width: '100%', height: '100%' }}>
+                <img src={props.background} alt={props.title} />
+            </div>
             <div style={{ textAlign: 'center', zIndex: 1, position: 'absolute', width: '100%', top: 0 }}>
                 <h3 className="title" style={{ zIndex: 1 }}>{props.title}</h3>
             </div>
 
             <div style={{ textAlign: 'center', zIndex: 1, position: 'absolute', width: '100%', bottom: 0 }}>
-                <Icon name='download' style={{ zIndex: 1 }} />
+                <Icon name='download' style={{ zIndex: 1, fontSize: '20pt', color: 'rgb(0, 68, 140)' }} />
             </div>
-
         </div>
     );
 };
