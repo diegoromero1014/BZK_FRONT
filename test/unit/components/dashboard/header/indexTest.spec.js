7f3d85855e60391025d21586f201df4630a84eb9
Alejandro García <jualcarv@bancolombia.com.co>
2020-03-10 10:03:17 -0500

    Prueba unitaria.

--- test/unit/components/dashboard/header/indexTest.spec.js
+++ test/unit/components/dashboard/header/indexTest.spec.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../../../../src/components/dashboard/header';
 
+let stubLocalStorage;
 
 describe('Header Test', () => {
 
     it('Should render component', () => {
+        stubLocalStorage = sinon.stub(window.sessionStorage, 'getItem').returns("Any Name");
         itRenders(<Header />);
     })
 })
