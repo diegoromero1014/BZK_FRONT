c0565a6c7484220e3b9ead2869a9d5da967b70fc
marios <marios@bancolombia.com.co>
2020-03-24 10:37:56 -0500

    avance

---
+++ src/components/managementView/SearchClient/constants.js
@@ -0,0 +1,24 @@
+export const TITLE_SEARCH_CLIENT = 'Buscar clientes';
+export const PLACEHOLDER_SEARCH_CLIENT = 'Buscar por nombre, NIT o grupo económico';
+export const HEADERS_CLIENT = [
+    {
+        name: 'Prospecto',
+        prop: ''
+    },
+    {
+        name: 'Tipo documuento',
+        prop: ''
+    },
+    {
+        name: 'Número de documento',
+        prop: ''
+    },
+    {
+        name: 'Razón social',
+        prop: ''
+    },
+    {
+        name: 'Grupo economico',
+        prop: ''
+    }
+]
--- src/components/managementView/SearchClient/index.js
+++ src/components/managementView/SearchClient/index.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import Tooltip from '../../toolTip/toolTipComponent';
+import { TITLE_SEARCH_CLIENT, PLACEHOLDER_SEARCH_CLIENT } from './constants';
 
-class SearchClient extends Component{
-    render(){
-        return(
-            <div>
-                <h4>Buscar clientes</h4>
-                <div>
-                    <input type="text"/>
-                    <button>Agregar</button>
+class SearchClient extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            
+        }
+    }
+
+
+    render() {
+        return (
+            <div style={{ margin: "30px 0px" }}>
+                <h3>{TITLE_SEARCH_CLIENT}</h3>
+                <div style={{ width: "100%" , display: 'flex'}}>
+                    <div style={{width: "80%"}}>
+                        <input type="text" style={{ padding: '0px 11px !important', width: "80%" }} placeholder={PLACEHOLDER_SEARCH_CLIENT}
+                            className="input-lg input InputAddOn-field" />
+                        <button id="searchClients" className="btn" title="Buscar clientes" type="button"
+                            style={{ backgroundColor: "#E0E2E2" }}>
+                            <i className="search icon" style={{ margin: '0em', fontSize: '1.2em' }} />
+                        </button>
+                    </div>
+                    <Tooltip text="Crear prospecto">
+                        <button className="btn btn-primary" onClick={this._clickButtonCreateProps} type="button"
+                            style={{ marginLeft: "60px" }}>
+                            <i className="add user icon"
+                                style={{ color: "white", margin: '0em', fontSize: '1.2em' }}></i>
+                        </button>
+                    </Tooltip>
                 </div>
             </div>
         )
