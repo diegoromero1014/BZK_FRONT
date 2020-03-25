7cdae0636c7c994ac3ffe5d8b2fd82618495a138
marios <marios@bancolombia.com.co>
2020-03-25 18:06:11 -0500

    avance, unit test searchClient

--- test/unit/components/managementView/widgets/searchClient/index.spec.js
+++ test/unit/components/managementView/widgets/searchClient/index.spec.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import SectionSearchClientRedux from '../../../../../../src/components/managementView/widgets/searchClient';
 import { SectionSearchClient } from '../../../../../../src/components/managementView/widgets/searchClient';
-import * as globalActions from '../../../../../../src/components/globalComponents/actions';;
+import * as globalActions from '../../../../../../src/components/globalComponents/actions';
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 import Immutable from 'immutable';
 
 let defaultProps;
-let  dispatchClearClients;
+let dispatchClearClients;
 let dispatchClientsFindServer;
 let dispatchSwtShowMessage;
 let redirectUrl;
@@ -19,11 +19,16 @@ const mockStore = configureStore(middlewares);
 describe('Test for SectionSearchClient Component', () => {
 
     beforeEach(() => {
-        store = mockStore({});
+        store = mockStore({
+            clientR: Immutable.Map({
+                responseClients: [],
+                countClients: 0
+            })
+        });
         dispatchClearClients = sinon.fake();
         dispatchClientsFindServer = sinon.fake();
         dispatchSwtShowMessage = sinon.fake();
-        redirectUrl = sinon.stub(globalActions, "redirectUrl"); 
+        redirectUrl = sinon.stub(globalActions, "redirectUrl");
 
         defaultProps = {
             data: [],
@@ -35,15 +40,15 @@ describe('Test for SectionSearchClient Component', () => {
         }
     });
 
-    
+
     afterEach(() => {
-        redirectUrl.restore();               
+        redirectUrl.restore();
     })
 
-    // Revisar
-    // it('Test rendering SectionSearchClient with Redux', () => {
-    //     itRenders(<SectionSearchClientRedux {...defaultProps} store={store} />)
-    // })
+
+    it('Test rendering SectionSearchClient with Redux', () => {
+        itRenders(<SectionSearchClientRedux store={store} />)
+    })
 
     it('Test rendering SectionSearchClient component', () => {
         itRenders(<SectionSearchClient {...defaultProps} />);
@@ -53,54 +58,53 @@ describe('Test for SectionSearchClient Component', () => {
 
         it('Test function setKeyword', () => {
             let keyword = "busqueda"
-            const wrapper = shallow(<SectionSearchClient {...defaultProps}/>);
+            const wrapper = shallow(<SectionSearchClient {...defaultProps} />);
             wrapper.setState({
                 keyword: "",
                 page: 1,
                 searched: false
             })
-            wrapper.instance().setKeyword(keyword); 
+            wrapper.instance().setKeyword(keyword);
             expect(wrapper.state().keyword).to.equal("busqueda");
         })
 
-        // Revisar
         it('Test function currentPage', () => {
-            let page = 1 ;
-            const wrapper = shallow(<SectionSearchClient {...defaultProps}/>);
-            wrapper.instance().currentPage(page); 
+            let page = 1;
+            const wrapper = shallow(<SectionSearchClient {...defaultProps} />);
+            wrapper.instance().currentPage(page);
             expect(wrapper.state().page).to.equal(1);
         })
 
         it('Test function currentPage', () => {
-            let page = 1 ;
-            const wrapper = shallow(<SectionSearchClient {...defaultProps}/>);
-            wrapper.instance().currentPage(page); 
+            let page = 1;
+            const wrapper = shallow(<SectionSearchClient {...defaultProps} />);
+            wrapper.instance().currentPage(page);
             expect(wrapper.state().page).to.equal(1);
         })
 
         it('Test function handelClickClient', () => {
             let element = {
-                id : 584946,
-                access : false
+                id: 584946,
+                access: false
             }
-            const wrapper = shallow(<SectionSearchClient {...defaultProps}/>);
+            const wrapper = shallow(<SectionSearchClient {...defaultProps} />);
             wrapper.instance().handelClickClient(element)
             expect(wrapper.length).to.equal(1);
         })
 
-        
+
         it('Test function handelClickClient', () => {
             let element = {
-                id : 584946,
-                access : true
+                id: 584946,
+                access: true
             }
-            const wrapper = shallow(<SectionSearchClient {...defaultProps}/>);
+            const wrapper = shallow(<SectionSearchClient {...defaultProps} />);
             wrapper.instance().handelClickClient(element)
             expect(redirectUrl.calledOnce).to.equal(true);
         })
 
         it('Test function restartPage', () => {
-            const wrapper = shallow(<SectionSearchClient {...defaultProps}/>);
+            const wrapper = shallow(<SectionSearchClient {...defaultProps} />);
             wrapper.setState({
                 keyword: "",
                 page: 10,
@@ -108,12 +112,12 @@ describe('Test for SectionSearchClient Component', () => {
             })
             wrapper.instance().restartPage();
             expect(wrapper.state().page).to.equal(1);
-            
+
         });
 
         it('Test function handleSetSearched ', () => {
-            let searched = true ;
-            const wrapper = shallow(<SectionSearchClient {...defaultProps}/>);
+            let searched = true;
+            const wrapper = shallow(<SectionSearchClient {...defaultProps} />);
             wrapper.setState({
                 keyword: "",
                 page: 10,
@@ -122,9 +126,5 @@ describe('Test for SectionSearchClient Component', () => {
             wrapper.instance().handleSetSearched(searched);
             expect(wrapper.state().searched).to.equal(true);
         })
-
-        // pendiente probar mapStateToProps y mapDispatchToProps
-
     })
-
 })
--- test/unit/components/managementView/widgets/searchClient/searchClient.spec.js
+++ test/unit/components/managementView/widgets/searchClient/searchClient.spec.js
@@ -1,3 +1,57 @@
 import React from 'react';
-import searchClientRedux from '../../../../../../src/components/managementView/widgets/searchClient/searchClient';
-import { searchClient } from '../../../../../../src/components/managementView/widgets/searchClient/searchClient';
+// import searchClientRedux from '../../../../../../src/components/managementView/widgets/searchClient/searchClient';
+// import { searchClient } from '../../../../../../src/components/managementView/widgets/searchClient/searchClient';
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
+
+
+let dispatchClientsFindServer ;
+let dispatchSwtShowMessage ; 
+let handleSetSearched ;
+let setKeyword ;
+let restartPage ;
+
+let defaultProps ;
+
+let store ;
+const middlewares = [thunk] ;
+const mockStore = configureStore(middlewares) ;
+
+describe('Test searchClient component', () => {
+
+    beforeEach(() => {
+        store = mockStore({});
+        dispatchClientsFindServer = sinon.fake();
+        dispatchSwtShowMessage = sinon.fake();
+        handleSetSearched = sinon.fake();
+        setKeyword = sinon.fake();
+        restartPage = sinon.fake();
+
+        defaultProps = {
+            dispatchClientsFindServer,
+            dispatchSwtShowMessage,
+            handleSetSearched,
+            setKeyword,
+            restartPage
+        }
+
+    })
+
+    describe('Rendering test', () => {
+        
+        it('Rendering searchClient component with Redux', () => {
+            itRenders(<searchClientRedux store={store}/>)
+        })
+
+        it('Rendering searchClient component', () => {
+            itRenders(<searchClient {...defaultProps}/>)
+        })
+
+    })
+
+    describe('test functions of searchClient component', () => {
+        
+    })
+    
+
+})
