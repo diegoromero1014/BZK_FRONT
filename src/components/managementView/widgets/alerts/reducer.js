8403448d6521b5927eca2ad05eef052638b5013b
Alejandro García <jualcarv@bancolombia.com.co>
2020-03-27 12:58:36 -0500

    Code smell

--- src/components/managementView/widgets/alerts/reducer.js
+++ src/components/managementView/widgets/alerts/reducer.js
@@ -6,9 +6,9 @@ const initialState = {
     rowCount: 0
 }
 
-export default (state = initialState, { type = null, payload = {} }) => {
-  if(type == OUTDATED_CONTACTS) {
-    return Object.assign({}, state, {rows: get(payload, 'data.data.rows', []), rowCount: get(payload, 'data.data.rowCount', 0)});
+export default (state = initialState, action = {}) => {
+  if(action.type == OUTDATED_CONTACTS) {
+    return Object.assign({}, state, {rows: get(action.payload, 'data.data.rows', []), rowCount: get(action.payload, 'data.data.rowCount', 0)});
   } else {
     return state;
   }
