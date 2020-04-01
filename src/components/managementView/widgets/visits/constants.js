96d28407840aa7a4587821446f78d7ba532313dd
rcadavi <rcadavi@bancolombia.com.co>
2020-04-01 17:02:22 -0500

    Cambio en el tamaño de los tabs

--- src/components/managementView/widgets/visits/constants.js
+++ src/components/managementView/widgets/visits/constants.js
@@ -1,2 +1,2 @@
 export const ECONOMIC_GROUP_TAB = 'Grupo económico que no se ha visitado en mas de tres meses';
-export const PENDING_VIEWS_TAB = 'Visitas pendientes';
+export const PENDING_VIEWS_TAB = 'Visitas pendientes en este mes';
