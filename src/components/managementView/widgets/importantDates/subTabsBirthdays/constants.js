ef7c5fe19d3610c9b07f7660f2585fe62cfa00fa
camandgo <camandgo@bancolombia.com.co>
2020-04-13 08:53:28 -0500

    avance

--- src/components/managementView/widgets/importantDates/subTabsBirthdays/constants.js
+++ src/components/managementView/widgets/importantDates/subTabsBirthdays/constants.js
@@ -1,4 +1,4 @@
-import { ViewDetailClient } from './viewDetailClient';
+import ViewDetailClient from './viewDetailClient';
 export const TITLE_STRATEGIC_CONTACTS = "Estratégicos";
 export const TITLE_TACTIC_CONTACTS = "Tácticos";
 export const TITLE_OPERATIVE_CONTACTS = "Operativos";
@@ -7,11 +7,18 @@ export const STRATEGIC_CONTACTS = "Estratégico";
 export const TACTIC_CONTACTS = "Táctico";
 export const OPERATIVE_CONTACTS = "Operativo";
 
-export const ACTION_STRATEGIC_CONTACTS = "ACTION_STRATEGIC_CONTACTS"; 
-export const ACTION_TACTIC_CONTACTS = "ACTION_TACTIC_CONTACTS"; 
-export const ACTION_OPERATIVE_CONTACTS = "ACTION_OPERATIVE_CONTACTS"; 
+export const ACTION_STRATEGIC_CONTACTS = "ACTION_STRATEGIC_CONTACTS";
+export const ACTION_TACTIC_CONTACTS = "ACTION_TACTIC_CONTACTS";
+export const ACTION_OPERATIVE_CONTACTS = "ACTION_OPERATIVE_CONTACTS";
 
 export const MAX_ROWS = 5;
+
+export const COLUMNS_CLIENTS = [
+    {
+        header: 'Razón social',
+        prop: 'name'
+    }
+]
 
 export const COLUMNS_CONTACTS = [
     {
@@ -45,7 +52,8 @@ export const COLUMNS_CONTACTS = [
         prop: 'clients',
         component: {
             Component: ViewDetailClient
-        }
+        },
+        width: 2
     },
 
 ]
--- src/components/managementView/widgets/importantDates/subTabsBirthdays/viewDetailClient.js
+++ src/components/managementView/widgets/importantDates/subTabsBirthdays/viewDetailClient.js
@@ -1,9 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Modal from 'react-modal';
 import { Icon } from 'semantic-ui-react';
-const ViewDetailClient = ({ data }) => (
-    <div>
-        <Icon name='users' />
-    </div>
-);
+import Table from "../../../../table";
+import TableBuilder from "../../../../table/TableBuilder";
+import { COLUMNS_CLIENTS } from './constants';
+
+class ViewDetailClient extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            visible: false,
+            name: '',
+            open: false,
+            clients: []
+        }
+    }
+
+    async componentWillMount() {
+        const { data = [] } = this.props;
+
+        let clients = data.split(";;");
+
+        if (clients.length > 1) {
+            clients = clients.map(client => ({ name: client }));
+
+            await this.setState({ visible: true, clients });
+        } else {
+            await this.setState({ visible: false, name: clients[0] });
+        }
+    }
+
+    handleCloseModal = () => this.setState({ open: false });
+
+    handleOnClick = () => this.setState({ open: true });
+
+    render() {
+        const { visible, name, open, clients } = this.state;
+
+        return (
+            <div>
+                {visible ? <Icon onClick={this.handleOnClick} name='users' /> : <p>{name}</p>}
+
+                <Modal isOpen={open} onRequestClose={this.handleCloseModal} className="modalBt4-fade modal fade contact-detail-modal in" style={{ zIndex: 100 }}>
+                    <div className="modalBt4-dialog modalBt4-lg" style={{ zIndex: 100 }}>
+                        <div className="modalBt4-content modal-content" style={{ zIndex: 100 }}>
+                            <div className="modalBt4-header modal-header">
+                                <h4 className="modal-title" style={{ float: 'left', marginBottom: '0px' }} id="myModalLabel">Clientes</h4>
+
+                                <button type="button" onClick={this.handleCloseModal} className="close" data-dismiss="modal" role="close">
+                                    <span className="modal-title" aria-hidden="true" role="close"><i className="remove icon modal-icon-close" role="close"></i></span>
+                                    <span className="sr-only">Close</span>
+                                </button>
+                            </div>
+
+                            <div
+                                style={{
+                                    width: '100%', display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    padding: '50px 0'
+                                }}
+                            >
+                                <div style={{ width: '70%', }} >
+                                    <Table
+                                        tableSettings={
+                                            new TableBuilder(clients, COLUMNS_CLIENTS)
+                                                .setNoRowMessage("No existen registros.")
+                                                .setRecordsPerPage(5)
+                                                .setStriped(true)
+                                                .setTotalRecords(clients.length)
+                                                .setOnPageChange(page => console.log(page))
+                                                .setMaximumVisiblePages(7)
+                                                .build()
+                                        }
+                                    />
+                                </div>
+                            </div>
+
+
+                            <div className="modalBt4-footer modal-footer">
+                                <button type="submit" className="btn btn-primary modal-button-edit" style={{ marginRight: 15 }}>
+                                    <span>Guardar</span>
+                                </button>
+
+                                <button type="button" className="btn btn-default modal-button-edit" onClick={this.handleCloseModal}>
+                                    <span>Cancelar</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </Modal>
+            </div>
+
+        );
+    }
+}
 
 export default ViewDetailClient;
