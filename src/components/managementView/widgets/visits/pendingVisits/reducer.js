699a4f258b6c60baca465fedb88e5a963a8444fb
marios <marios@bancolombia.com.co>
2020-04-14 09:50:23 -0500

    organizacion de una linea muy larga que podria ser reportada por sonar.

--- src/components/managementView/widgets/visits/pendingVisits/reducer.js
+++ src/components/managementView/widgets/visits/pendingVisits/reducer.js
@@ -12,7 +12,12 @@ const pendingVisits = (state = initialState, action = {}) => {
 
 	if (action.type == REQUEST_PENDING_VISITS) {
 		let newState = Object.assign({}, state, { rows: get(action.payload, 'data.data.rows', []), rowCount: get(action.payload, 'data.data.rowCount', 0) })
-		const newRows = newState.rows.map(element => element = Object.assign({}, element, { visitTime: mapDateValueFromTaskByFormat(element.visitTime, REVIEWED_DATE_FORMAT_HOUR) }))
+		const newRows = newState.rows.map(element => 
+			element = Object.assign(
+				{},
+				element, 
+				{ visitTime: mapDateValueFromTaskByFormat(element.visitTime, REVIEWED_DATE_FORMAT_HOUR) })
+			)
 		return Object.assign({}, newState, { rows: newRows })
 	} else {
 		return state;
