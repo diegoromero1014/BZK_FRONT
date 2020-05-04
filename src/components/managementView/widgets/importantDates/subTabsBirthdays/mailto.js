cdc89f4ec11a57cb6b21e0eadce882e34dcbfefc
camandgo <camandgo@bancolombia.com.co>
2020-05-04 12:41:24 -0500

    avance

--- src/components/managementView/widgets/importantDates/subTabsBirthdays/mailto.js
+++ src/components/managementView/widgets/importantDates/subTabsBirthdays/mailto.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { capitalize } from 'lodash';
 import { NAME } from './constants';
 
 const MailTo = ({ data, type }) => (
-    <a href={`mailto:${data.email}?Subject=¡Feliz cumpleaños ${capitalize(data.contactName)}!`}>
+    <a href={`mailto:${data.email}?Subject=¡Feliz cumpleaños!`}>
         {type === NAME ? data.contactName : data.contactLastName}
     </a>
 );
