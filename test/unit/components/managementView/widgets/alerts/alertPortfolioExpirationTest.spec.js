0a6e2c8e84f97f960c03550e1552ab1a42b84000
Alejandro García <jualcarv@bancolombia.com.co>
2020-05-19 10:19:47 -0500

    unit test

--- test/unit/components/managementView/widgets/alerts/alertPortfolioExpirationTest.spec.js
+++ test/unit/components/managementView/widgets/alerts/alertPortfolioExpirationTest.spec.js
@@ -4,8 +4,12 @@ import {AlertPortfolioExpiration } from '../../../../../../src/components/manage
 import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 import Immutable from 'immutable';
+import * as globalActions from '../../../../../../src/components/globalComponents/actions';
 
 let defaultProps;
+let dispatchGetAlertPortfolioExpirationDashboard;
+let dispatchChangeActiveItemMenu;
+let redirectUrl;
 
 let store;
 const middleWares = [thunk];
@@ -14,8 +18,13 @@ const mockStore = configureStore(middleWares);
 describe('AlertPortfolioExpiration Test', () => {
     
     beforeEach(() => {
+        dispatchGetAlertPortfolioExpirationDashboard = sinon.fake();
+        dispatchChangeActiveItemMenu = sinon.fake();
+        redirectUrl = sinon.stub(globalActions, "redirectUrl");
         defaultProps = {
-            data: []
+            data: [],
+            dispatchGetAlertPortfolioExpirationDashboard,
+            dispatchChangeActiveItemMenu
         }
         store = mockStore({
             alertPortfolioExpiration: Immutable.Map({ responseClients: {} }),
@@ -28,6 +37,10 @@ describe('AlertPortfolioExpiration Test', () => {
         });
     })
 
+    afterEach(() => {
+        redirectUrl.restore()
+    })
+
     it('Should render component with Redux', () => {
         itRenders(<AlertPortfolioExpirationRedux {...defaultProps} store={store} />);
     })
@@ -35,4 +48,33 @@ describe('AlertPortfolioExpiration Test', () => {
     it('Should render component', () => {
         itRenders(<AlertPortfolioExpiration {...defaultProps}/>);
     })
+    
+    it("When handleOnPageChange is instanced", async () => {
+        const wrapper = shallow(<AlertPortfolioExpiration {...defaultProps} />);
+        wrapper.setState({ loading: null });
+        await wrapper.instance().handleOnPageChange(1);
+        expect(wrapper.state().loading).to.equal(false);
+    })
+    
+    it("When setLoading is instanced", async () => {
+        const wrapper = shallow(<AlertPortfolioExpiration {...defaultProps} />);
+        wrapper.setState({ loading: null });
+        await wrapper.instance().setLoading(true);
+        expect(wrapper.state().loading).to.equal(true);
+    })
+
+    it("When redirectToAlertPortfolioExpiration is instanced", () => {
+        const wrapper = shallow(<AlertPortfolioExpiration {...defaultProps} />);
+        wrapper.setState({ loading: null });
+        wrapper.instance().redirectToAlertPortfolioExpiration();
+        sinon.assert.called(dispatchChangeActiveItemMenu);
+    })
+    
+    it('When component did updated is instanced', () => { 
+        const wrapper = shallow(<AlertPortfolioExpiration {...defaultProps} />);
+        wrapper.setState({ loading: null });
+        wrapper.setProps({ idFilter: 1 });
+        expect(wrapper.state().loading).to.equal(true);
+    })
+
 })
