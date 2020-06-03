d13f6a5e885665231bcfad2e9df768df96199dbd
Edwin Otalvaro <ealejandro.otalvaro@gmail.com>
2020-06-03 16:36:23 -0500

    pruebas unitarias a interceptor

--- src/api/index.js
+++ src/api/index.js
@@ -8,18 +8,23 @@ export function setAuthorizationHeader(token) {
     }
 }
 
-export function setInterceptors() {
-    axios.interceptors.response.use(function (response) {
-        return response;
-    }, function (error) {
+export function successInterceptor(response) {
+    return response;
+}
+
+export function errorInterceptor(historyRouter) {
+    return function(error) {
         if (401 === error.response.status) {
-            localStorage.setItem("sessionTokenFront", "");
-            router.push('/login')
+            window.localStorage.setItem("sessionTokenFront", "");
+            historyRouter.push('/login')
         } else if (403 === error.response.status) {
-            alert("Señor usuario, no tiene permisos para visualizar este modulo");
-            router.push("/dashboard")
+            historyRouter.push("/dashboard")
         } else {
             return Promise.reject(error);
         }
-    });
+    }
+}
+
+export function setInterceptors() {
+    axios.interceptors.response.use(successInterceptor, errorInterceptor(router));
 }
